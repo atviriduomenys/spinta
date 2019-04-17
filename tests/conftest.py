0c830d29b902e913a6c8bc36d911b3478719f57e
Justas Trimailovas <trymas@pov.lt>
2019-04-17 11:20:15 +0300

    Fix typo

--- tests/conftest.py
+++ tests/conftest.py
@@ -137,7 +137,7 @@ def context(postgresql):
         for name in models:
             context.wipe(name)
 
-    # Datasets does not have foreign kei constraints, so there is no need to
+    # Datasets does not have foreign key constraints, so there is no need to
     # topologically sort them. At least for now.
     for dataset in store.manifests['default'].objects['dataset'].values():
         for model in dataset.objects.values():
