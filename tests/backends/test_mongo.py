92984473289df489e670f95bf15f2b02c260af74
Justas Trimailovas <trymas@pov.lt>
2019-04-18 10:53:04 +0300

    Create tests for mongo insert and update

--- tests/backends/test_mongo.py
+++ tests/backends/test_mongo.py
@@ -1,23 +1,60 @@
-def test_schema_loader(context):
+def test_mongo_insert_get(context):
     # Add few objects to database.
     result = context.push([
         {
-            'type': 'mongo',
-            'value': '42',
+            'type': 'report',
+            'status': '42',
         },
     ])
 
     result = {x.pop('type'): x for x in result}
+
+    # mongo inserts ID at key `_id` and we also insert ID at key `id`, look up
+    # code at: spinta/types/store.py::push
     assert result == {
-        'mongo': {
-            'id': result['mongo']['id'],
-            'value': '42',
+        'report': {
+            'id': result['report']['id'],
+            'status': '42',
         },
     }
 
     # Read those objects from database.
-    assert context.getone('mongo', result['mongo']['id']) == {
-        'type': 'mongo',
-        'id': result['mongo']['id'],
-        'value': '42',
+    get_data = context.getone('report', result['report']['id'])
+    assert get_data == {
+        'id': result['report']['id'],
+        'status': '42',
+    }
+
+
+def test_mongo_update_get(context):
+    # Add few objects to database.
+    insert_result = context.push([
+        {
+            'type': 'report',
+            'status': '42',
+        },
+    ])
+
+    # change report status
+    report_data = list(insert_result)[0]
+    report_data['status'] = '13'
+
+    # push updated report to database
+    update_result = context.push([report_data])
+    update_result = {x.pop('type'): x for x in update_result}
+
+    # mongo inserts ID at key `_id` and we also insert ID at key `id`, look up
+    # code at: spinta/types/store.py::push
+    assert update_result == {
+        'report': {
+            'id': update_result['report']['id'],
+            'status': '13',
+        },
+    }
+
+    # Read those objects from database.
+    get_data = context.getone('report', update_result['report']['id'])
+    assert get_data == {
+        'id': update_result['report']['id'],
+        'status': '13',
     }
