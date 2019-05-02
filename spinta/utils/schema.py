7652be1f0a71e600d4bd62b3b6fc907df7201292
Justas Trimailovas <j.trimailovas@gmail.com>
2019-05-02 12:10:49 +0300

    Create a description for `resolve_schema`
    You could not understood what it does just by galncing at this function

--- spinta/utils/schema.py
+++ spinta/utils/schema.py
@@ -1,4 +1,20 @@
 def resolve_schema(obj, Base):
+    # Emulates inheritance for `schema` class attribute for given `obj` and
+    # one of the `obj's` base classes `Base`.
+    #
+    # e.g.:
+    #
+    # class Base:
+    #     schema = { 'required': {'type': 'bool', 'default': False} }
+    #
+    # class Obj(Base):
+    #     schema = { 'items': {} }
+    #
+    # obj = Obj()
+    # full_schema = resolve_schema(obj, Base)
+    # print(full_schema)
+    # { 'required': {'type': 'bool', 'default': False}, 'items': {} }
+
     bases = []
     for cls in obj.__class__.mro():
         bases.append(cls)
