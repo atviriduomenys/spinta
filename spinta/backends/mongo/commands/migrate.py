1aa7b402e99eaace1df34b383b7f11eb31487505
Mantas <sirexas@gmail.com>
2020-04-17 12:36:14 +0300

    Execute migration actions

--- spinta/backends/mongo/commands/migrate.py
+++ spinta/backends/mongo/commands/migrate.py
@@ -1,8 +1,9 @@
 from spinta import commands
 from spinta.components import Context
+from spinta.manifests.components import Manifest
 from spinta.backends.mongo.components import Mongo
 
 
-@commands.migrate.register()
-def migrate(context: Context, backend: Mongo):
+@commands.migrate.register(Context, Manifest, Mongo)
+def migrate(context: Context, manifest: Manifest, backend: Mongo):
     pass
--- spinta/manifests/backend/commands/bootstrap.py
+++ spinta/manifests/backend/commands/bootstrap.py
@@ -10,5 +10,7 @@ from spinta.manifests.backend.helpers import run_bootstrap
 def bootstrap(context: Context, manifest: BackendManifest):
     backend = manifest.backend
     if not backend.bootstrapped():
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(run_bootstrap(context, manifest))
+        with context:
+            context.attach('transaction', manifest.backend.transaction, write=True)
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(run_bootstrap(context, manifest))
