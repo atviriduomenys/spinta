b2e8e87ea0810f31e8a54b59435d2c2d1dcdf861
Mantas <sirexas@gmail.com>
2021-01-27 14:22:22 +0200

    Replace snoop with my own version
    snoop is too noise, generated large indentation, prints time in the
    output, does not always print given given argument expression.
    
    And it turned out, that reimplementing same functionality, at least pp
    part, was pretty easy.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,48 @@
+import builtins
+import inspect
 import sys
+from itertools import chain
+from itertools import islice
+from typing import Iterator
 
-import snoop
-import pprint
 import pprintpp
+import sqlparse
+from pygments import highlight
+from pygments.formatters.terminal256 import Terminal256Formatter
+from pygments.lexers.python import Python3Lexer
+from pygments.lexers.sql import PostgresLexer
 
-# See: https://github.com/alexmojaki/snoop
-snoop.install(
-    # Force colors, since pytest captures all output by default.
-    color=True,
-    # Some tests mock sys.stderr, to we need to pass it directly.
-    out=sys.stderr,
-)
 
-# User pprintpp for nicer and more readable output.
-# https://github.com/alexmojaki/snoop/issues/13
-pprint.pformat = pprintpp.pformat
+def formatter():
+    return Terminal256Formatter(style='vim')
+
+
+def ppsql(qry):
+    sql = str(qry) % qry.compile().params
+    sql = sqlparse.format(sql, reindent=True, keyword_case='upper')
+    sql = highlight(sql, PostgresLexer(), formatter())
+    print(sql)
+
+
+def pp(obj):
+    if isinstance(obj, Iterator):
+        out = list(islice(obj, 10))
+        out = '<generator> ' + pprintpp.pformat(out)
+        obj = chain(out, obj)
+    else:
+        out = pprintpp.pformat(obj)
+    frame = inspect.currentframe()
+    frame = inspect.getouterframes(frame)[1]
+    line = inspect.getframeinfo(frame[0]).code_context[0].strip()
+    if line.endswith(')'):
+        arg = line[line.find('(') + 1:-1]
+        out = f'{arg} = {out}'
+    out = highlight(out, Python3Lexer(), formatter())
+    print(out, file=sys.__stderr__)
+    return obj
+
+
+pp.sql = ppsql
+builtins.pp = pp
 
 pytest_plugins = ['spinta.testing.pytest']
