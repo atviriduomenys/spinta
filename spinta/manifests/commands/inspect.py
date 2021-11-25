68c566fa4290a74e5fc4b4569975436d111d8fde
Mantas <sirexas@gmail.com>
2021-11-25 10:36:39 +0200

    Remove print_methods debug call

--- spinta/manifests/commands/inspect.py
+++ spinta/manifests/commands/inspect.py
@@ -35,5 +35,4 @@ def inspect(
     }
     yield eid, schema
     for resource in dataset.resources.values():
-        commands.inspect.print_methods(context, resource.backend, resource, None)
         yield from commands.inspect(context, resource.backend, resource, None)
