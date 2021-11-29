6f14053d3405991689f4e909942aa00cad80e4d8
Mantas <sirexas@gmail.com>
2021-11-29 16:24:57 +0200

    Refactor getall[Context, Request, Model, Backend]

--- spinta/commands/read.py
+++ spinta/commands/read.py
@@ -1,19 +1,59 @@
 from starlette.requests import Request
 
 from spinta import commands
+from spinta.backends import Backend
+from spinta.backends import get_select_prop_names
+from spinta.backends import get_select_tree
+from spinta.compat import urlparams_to_expr
 from spinta.components import Context, Node, Action, UrlParams
+from spinta.components import Model
+from spinta.datasets.components import ExternalBackend
+from spinta.renderer import render
 
 
-@commands.getall.register(Context, Request, Node)
+@commands.getall.register(Context, Request, Model, Backend)
 async def getall(
     context: Context,
     request: Request,
-    node: Node,
+    model: Model,
+    backend: Backend,
     *,
     action: Action,
     params: UrlParams,
 ):
-    return await commands.getall(context, request, node, node.backend, action=action, params=params)
+    commands.authorize(context, action, model)
+    if isinstance(backend, ExternalBackend):
+        # XXX: `add_count` is a hack, because, external backends do not
+        #      supports it yet.
+        expr = urlparams_to_expr(params, add_count=False)
+    else:
+        expr = urlparams_to_expr(params)
+
+    accesslog = context.get('accesslog')
+    accesslog.log(
+        model=model.model_type(),
+        action=action.value,
+    )
+
+    rows = commands.getall(context, model, model.backend, query=expr)
+    if not params.count:
+        select_tree = get_select_tree(context, action, params.select)
+        prop_names = get_select_prop_names(context, model, action, select_tree)
+        rows = (
+            commands.prepare_data_for_response(
+                context,
+                action,
+                model,
+                backend,
+                row,
+                select=select_tree,
+                prop_names=prop_names,
+            )
+            for row in rows
+        )
+    return render(context, request, model, params, rows, action=action)
+
+
 
 
 @commands.getone.register(Context, Request, Node)
