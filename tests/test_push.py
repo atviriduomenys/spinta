3f87598ba95b5f2efc41320f9a9bdaebfa192ec2
Mantas <sirexas@gmail.com>
2022-04-28 09:45:43 +0300

    Refactoring push command
    Current push implementetion can't resolve circular dependencies. Also,
    current implementation reads all rows from source table, which can lock
    table.
    
    New implementation:
    
    - will read rows one by one using threads;
    - each read can be postponed if dependencies are not yet pushed;
    - source tables will be read in small chunks;
    - there will be no need to set_matadata scope since _id's will be taken
      from target data store.
    
    Fixes:
    
    - https://github.com/atviriduomenys/spinta/issues/149

--- tests/test_push.py
+++ tests/test_push.py
@@ -1,7 +1,22 @@
+import datetime
 import json
 import hashlib
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Union
 
 import pytest
+
+from spinta.components import Model
+from spinta.components import Property
+from spinta.core.config import RawConfig
+from spinta.testing.manifest import load_manifest_and_context
+from spinta.types.datatype import DataType
+from spinta.types.datatype import Ref
+from spinta.types.datatype import Object
+from spinta.types.datatype import Array
 
 
 @pytest.mark.skip('datasets')
@@ -89,3 +104,118 @@ def test_push_different_models(app):
         'update_time': None,
         'valid_from_date': None,
     }
+
+
+class PrimaryKey:
+    model: str       # Model absolute name
+    keys: List[str]  # List of property names uniquely identifying an object
+    last: Dict[str, Any]  # Last pushed object
+    time: datetime.datetime  # Time of last push
+
+
+class PrimaryId:
+    key: PrimaryKey
+    timestamp: int   # Last pushed time, unix timestamp (0 if never pushed)
+    external: str    # UUID of an external global object
+    internal: str    # SHA1 of and internal unique object identifier
+    checksum: str    # SHA1 checksum of whole object.
+
+
+class SecondaryKey:
+    model: str       # Model absolute name
+    keys: List[str]  # List of property names uniquely identifying an object
+
+
+class SecondaryId:
+    key: SecondaryKey
+    external: str    # UUID of an external global object
+    internal: str    # SHA1 of an internal unique object identifier
+
+
+def get_object_id(model: Model, data: Dict[str, Any]) -> Optional[PrimaryId]:
+    """Find external object id by given model and object data
+
+    Returns None if external id is not found.
+    """
+    return ''
+
+
+def get_object_ref_id(dtype: Ref, data: Dict[str, Any]) -> Optional[SecondaryId]:
+    """Find external object id by given ref and object data
+
+    If ref uses same keys as primarky key, then get_object_id will be used,
+    otherwise, secondary key will be used.
+
+    Returns None if external id is not found.
+    """
+    return ''
+
+
+def create_object_id(model: Model, data: Dict[str, Any]) -> PrimaryId:
+    """Create and external object id for given object data
+
+    `_id` must be present in `data`, it will be used as external id.
+
+    This function will also creates all secondary keys refered to this model,
+    if refered keys do not match primary key.
+    """
+    return PrimaryId()
+
+
+def update_object_id(model: Model, data: Dict[str, Any]) -> PrimaryId:
+    """Update external id of an object by given data
+
+    `_id` must be present in `data`, it will be used as external id.
+
+    This will also updates all secondary keys refered to this model, if refered
+    keys do not match primary key.
+    """
+    oid = get_object_id(model, data)
+    if oid is None:
+        create_object_id()
+    return PrimaryId()
+
+
+def ref_objects_pushed(node: Union[Model, DataType], data: Any) -> bool:
+    if isinstance(node, Model):
+        for prop in node.properties.values():
+            if not ref_objects_pushed():
+                return False
+    elif isinstance(node, Object):
+        for prop in node.properties.values():
+            if not ref_objects_pushed(prop.dtype):
+                return False
+    elif isinstance(node, Array):
+        if not ref_objects_pushed(node.items.dtype):
+            return False
+    elif isinstance(node, Ref):
+        if get_object_ref_id() is None:
+            return False
+    return True
+
+
+def push_single_object(model: Model, data: Dict[str, Any]) -> bool:
+    # Check if referenced objects already pushed.
+    if not ref_objects_pushed(model, data):
+        return False
+
+    oid = get_object_id(model, data)
+
+    return True
+
+
+
+def test_push_v2(rc: RawConfig):
+    context, manifest = load_manifest_and_context(rc, '''
+    d | r | b | m | property   | type    | ref     | source | access
+    example                    |         |         |        |
+      |   |   | City           |         | id      | CITY   |
+      |   |   |   | id         | integer |         | ID     | open
+      |   |   |   | name       | string  |         | NAME   | open
+    ''')
+    model = manifest.models['example/City']
+    data = {
+        '_type': 'example/City',
+        'name': 'Vilnius',
+    }
+    assert push_single_object(model, data)
