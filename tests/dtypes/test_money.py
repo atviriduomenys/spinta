d384715e75e22f50f753e61f13aaae1d92c57ad3
Gediminas <gediminas.msc@gmail.com>
2022-11-15 09:49:20 +0200

    Change amount type in money test.

--- tests/dtypes/test_money.py
+++ tests/dtypes/test_money.py
@@ -15,7 +15,7 @@ def test_money(
     backends/postgres/dtypes/money             |                |
       |   |   | City                           |                |
       |   |   |   | name                       | string         |
-      |   |   |   | amount                     | money          | 
+      |   |   |   | amount                     | integer        | 
     ''', backend=postgresql, request=request)
 
     model: str = 'backends/postgres/dtypes/money/City'
