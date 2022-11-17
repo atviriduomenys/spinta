4bd56536cb57da2304498904c60745cadef01b48
Gediminas <gediminas.msc@gmail.com>
2022-11-17 14:34:08 +0200

    Changes for the dataset, model, property name checking.

--- spinta/datasets/commands/check.py
+++ spinta/datasets/commands/check.py
@@ -1,34 +1,45 @@
 from spinta import commands
 from spinta.components import Context
 from spinta.datasets.components import Dataset, Resource
-from spinta.components import Model
 import re
 
 
 @commands.check.register()
 def check(context: Context, dataset: Dataset):
-    rc = context.get('rc')
-    if rc.get('names'):
-        name = dataset.name
-        lowercase = len([i for i in name if i.islower()] + re.findall('/', name)) == len(name)
-        if not lowercase or name.startswith('/') or name.endswith('/') or re.findall('//', name):
-            raise Exception(f'Dataset name {name} is not correct.')
     for resource in dataset.resources.values():
         commands.check(context, resource)
+
+    rc = context.get('rc')
+
+    if rc.get('names'):
+        name = dataset.name
+        is_lowercase = len([i for i in name if i.islower()] + re.findall('/', name)) == len(name)
+
+        # Dataset name check
+        if not is_lowercase or name.startswith('/') or name.endswith('/') or re.findall('//', name):
+            raise Exception(f'Dataset name {name} is not correct.')
+
+        store = context.get('store')
+        models = store.manifest.models
+
+        for name, model in models.items():
+            if not name.startswith('_'):
+                name = name.rsplit('/', 1)[-1]
+                is_lowercase = name.lower() == name
+                is_uppercase = name.upper() == name
+
+                # Model name check
+                if not re.findall(r'^[A-Za-z]\w*$', name) or is_lowercase or is_uppercase:
+                    raise Exception(f'Model name {name} is not correct.')
+
+            for prop_name, prop in model.properties.items():
+
+                # Property name check
+                if re.findall(r'^[A-Z]\w*$', prop_name):
+                    raise Exception(f'Property name {prop_name} is not correct.')
 
 
 @commands.check.register()
 def check(context: Context, resource: Resource):
     pass
 
-
-@commands.check.register()
-def check(context: Context, model: Model):
-    rc = context.get('rc')
-    if rc.get('names'):
-        name = model.name
-        if name.lower() != name and name.upper() != name and '_' in name:
-            raise Exception(f'Model name {name} is not correct.')
-        for name, prop in model.properties.items():
-            if re.findall(r'^[A-Z]\w*$', name):
-                raise Exception(f'Property name {name} is not correct.')
