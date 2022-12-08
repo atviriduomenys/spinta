be4437378d93f0a523939f0d64652f430f0577b0
Gediminas <gediminas.msc@gmail.com>
2022-12-08 22:18:59 +0200

    Changes in for dtype Money adjustments.

--- spinta/types/money/link.py
+++ spinta/types/money/link.py
@@ -10,5 +10,6 @@ from spinta.types.helpers import set_dtype_backend
 def link(context: Context, dtype: Money) -> None:
     set_dtype_backend(dtype)
     enum = dtype.prop.given.enum
-    if len(enum) > 3:
-        raise(UndefinedEnum(dtype.prop, name=enum))
+    if enum:
+        if len(enum) > 3:
+            raise(UndefinedEnum(dtype.prop, name=enum))
