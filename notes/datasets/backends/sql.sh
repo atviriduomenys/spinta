3ec2245f08b5e29ee3a89b83cbaa3e25d7e02f9a
Mantas <sirexas@gmail.com>
2023-03-22 16:00:01 +0200

    Add more tests to actually push data

--- notes/datasets/backends/sql.sh
+++ notes/datasets/backends/sql.sh
@@ -94,5 +94,65 @@ http GET "$SERVER/$DATASET/City"
 #| }
 
 
+# notes/docker.sh           Start docker compose
+# notes/spinta/server.sh    Add client
+# notes/postgres.sh         Show tables
+
+cat $BASEDIR/config.yml
+cat > $BASEDIR/config.yml <<EOF
+env: production
+data_path: $PWD/$BASEDIR
+default_auth_client: default
+
+keymaps:
+  default:
+    type: sqlalchemy
+    dsn: sqlite:///$PWD/$BASEDIR/keymap.db
+
+backends:
+  default:
+    type: postgresql
+    dsn: postgresql://admin:admin123@localhost:54321/spinta
+  mydb:
+    type: sql
+    dsn: postgresql://postgres:secret@localhost:5432/postgres
+
+manifest: default
+manifests:
+  default:
+    type: csv
+    path: $PWD/$BASEDIR/manifest.csv
+    backend: default
+    keymap: default
+    mode: internal
+
+accesslog:
+  type: file
+  file: $PWD/$BASEDIR/accesslog.json
+EOF
+
+poetry run spinta show
+poetry run spinta wait 1
+poetry run spinta bootstrap
+
+psql -h localhost -p 54321 -U admin spinta -c '\dt public.*'
+
+test -n "$PID" && kill $PID
+poetry run spinta run &>> $BASEDIR/spinta.log &; PID=$!
+tail -50 $BASEDIR/spinta.log
+
+# notes/spinta/client.sh    Configure client
+# notes/spinta/push.sh      Configure client
+
+poetry run spinta push $BASEDIR/manifest.csv -o test@localhost
+#| PUSH: 100% 2/2 [00:00<00:00, 81.44it/s]
+
+http GET "$SERVER/$DATASET/City?format(ascii)"
+#| _type                       _id                                   _revision                             code  name   
+#| --------------------------  ------------------------------------  ------------------------------------  ----  -------
+#| datasets/backends/sql/City  ebccf7c4-6bfa-4b46-9a1e-8c5302068caf  543f4787-e959-4f99-ae46-0f986006db48  1     Vilnius
+#| datasets/backends/sql/City  d0bcae75-4013-4bc2-841a-1c71309bc910  3435e034-d375-4cf6-973c-4b7495d45216  2     Kaunas
+
+
 test -n "$PID" && kill $PID
 docker stop postgres
