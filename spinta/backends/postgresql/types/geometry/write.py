ee5957711084b888f7640d7ba06a465b1c03de6d
Justinas Kenstavicius <adp@atea.lt>
2023-04-17 10:43:03 +0300

    418 Fix check while patch is empty

--- spinta/backends/postgresql/types/geometry/write.py
+++ spinta/backends/postgresql/types/geometry/write.py
@@ -2,6 +2,8 @@ from spinta import commands
 from spinta.components import Context, DataSubItem
 from spinta.backends.postgresql.components import PostgreSQL
 from spinta.types.geometry.components import Geometry
+from spinta.utils.data import take
+
 
 @commands.before_write.register(Context, Geometry, PostgreSQL)
 def before_write(
@@ -10,11 +12,9 @@ def before_write(
     backend: PostgreSQL,
     *,
     data: DataSubItem):
-    patch = {}
-    if 'SRID' not in data.patch and dtype.srid is not None:
-        patch = {dtype.prop.name: 'SRID='+str(dtype.srid)+';'+str(data.patch)}
-    else:
-        patch = {dtype.prop.name: str(data.patch)}
+    patch = take(all, {dtype.prop.place: data.patch})
+    if patch.get(dtype.prop.place) is not None and 'SRID' not in data.patch and dtype.srid is not None:
+        patch[dtype.prop.place] = f"SRID={dtype.srid};{data.patch}"
     return patch
 
 
