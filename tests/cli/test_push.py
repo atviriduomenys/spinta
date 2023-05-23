79dadc23b01f5c6e6439ec02816d5265de7b8352
adp <adp@atea.lt>
2023-05-23 16:23:19 +0300

    Added test to check for correct return of country code

--- tests/cli/test_push.py
+++ tests/cli/test_push.py
@@ -15,12 +15,20 @@ def geodb():
             sa.Column('id', sa.Integer, primary_key=True),
             sa.Column('kodas', sa.Text),
             sa.Column('pavadinimas', sa.Text),
+        ],
+        'cities': [
+            sa.Column('id', sa.Integer, primary_key=True),
+            sa.Column('name', sa.Text),
+            sa.Column('country', sa.Text),
         ]
     }) as db:
         db.write('salis', [
             {'kodas': 'lt', 'pavadinimas': 'Lietuva'},
             {'kodas': 'lv', 'pavadinimas': 'Latvija'},
             {'kodas': 'ee', 'pavadinimas': 'Estija'},
+        ])
+        db.write('cities', [
+            {'name': 'Vilnius', 'country': 'lt'},
         ])
         yield db
 
@@ -153,3 +161,62 @@ def test_push_with_resource_check(
     assert len(listdata(resp_res)) == 3
     assert len(listdata(resp_no_res)) == 0
 
+
+def test_push_ref_with_level(
+    postgresql,
+    rc,
+    cli: SpintaCliRunner,
+    responses,
+    tmp_path,
+    geodb,
+    request
+):
+    create_tabular_manifest(tmp_path / 'manifest.csv', striptable('''
+    d | r | b | m | property | type    | ref                             | source   | level | access
+    leveldataset             |         |                                 |          |       |
+      | db                   | sql     |                                 |          |       |
+      |   |   | City         |         | id                              | cities   | 4     |
+      |   |   |   | id       | integer |                                 | id       | 4     | open
+      |   |   |   | name     | string  |                                 | name     | 2     | open
+      |   |   |   | country  | ref     | /leveldataset/countries/Country | country  | 3     | open
+      |   |   |   |          |         |                                 |          |       |
+    leveldataset/countries   |         |                                 |          |       |
+      |   |   | Country      |         | code                            |          | 4     |
+      |   |   |   | code     | string  |                                 |          | 4     | open
+      |   |   |   | name     | string  |                                 |          | 2     | open
+    '''))
+
+    # Configure local server with SQL backend
+    localrc = create_rc(rc, tmp_path, geodb)
+
+    # Configure remote server
+    remote = configure_remote_server(cli, localrc, rc, tmp_path, responses)
+    request.addfinalizer(remote.app.context.wipe_all)
+
+    # Push data from local to remote.
+    assert remote.url == 'https://example.com/'
+    result = cli.invoke(localrc, [
+        'push',
+        '-d', 'leveldataset',
+        '-o', remote.url,
+        '--credentials', remote.credsfile,
+        '--no-progress-bar',
+    ])
+    assert result.exit_code == 0
+
+    result = cli.invoke(localrc, [
+        'push',
+        '-d', 'leveldataset/countries',
+        '-o', remote.url,
+        '--credentials', remote.credsfile,
+        '--no-progress-bar',
+    ])
+    assert result.exit_code == 0
+
+    remote.app.authmodel('leveldataset/City', ['getall', 'search'])
+    resp_city = remote.app.get('leveldataset/City')
+
+    assert resp_city.status_code == 200
+    assert listdata(resp_city, 'name') == ['Vilnius']
+    assert len(listdata(resp_city, 'id', 'name', 'country')) == 1
+    assert 'code' in listdata(resp_city, 'country')[0].keys()
