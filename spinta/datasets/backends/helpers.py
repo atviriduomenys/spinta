d7f4880b468b80300b57659db764baa43ea84cd6
Justinas Kenstavicius <adp@atea.lt>
2023-05-29 11:37:21 +0300

    460 Added tests, fixed logic

--- spinta/datasets/backends/helpers.py
+++ spinta/datasets/backends/helpers.py
@@ -12,11 +12,15 @@ def handle_ref_key_assignment(keymap: KeyMap, value: Any, prop: Property) -> dic
     else:
         if len(prop.dtype.refprops) > 1 and not prop.model.external.unknown_primary_key:
             raise NotImplementedFeature(prop, feature="Ability to have multiple refprops")
-        if prop.model.external.unknown_primary_key:
-            val = keymap.encode(prop.dtype.model.model_type(), value)
-            val = {'_id': val}
-        else:
+        if prop.dtype.refprops:
             val = {
                 prop.dtype.refprops[0].name: value
             }
+        elif not prop.model.external.unknown_primary_key and prop.model.external.pkeys:
+            val = {
+                prop.model.external.pkeys[0].name: value
+            }
+        else:
+            val = keymap.encode(prop.dtype.model.model_type(), value)
+            val = {'_id': val}
     return val
