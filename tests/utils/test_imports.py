e828f9916c61e99d02c2e4765ef2eb1aa4accae9
adp <adp@atea.lt>
2023-07-04 15:04:14 +0300

    78 Added tests for new use function

---
+++ tests/utils/test_imports.py
@@ -0,0 +1,23 @@
+import importlib
+from spinta.exceptions import ModuleNotInGroup
+import pytest
+from spinta.utils.imports import use
+
+
+def test_use_correct_simple():
+    sa = use('postgres', 'sqlalchemy')
+    module = importlib.import_module('sqlalchemy')
+    assert sa == module
+
+
+def test_use_correct_specific_package():
+    inspector = use('postgres', 'sqlalchemy.engine.reflection', 'Inspector')
+    module = importlib.import_module('sqlalchemy.engine.reflection', package='Inspector')
+    assert inspector == module
+
+
+def test_use_non_existant_package():
+    with pytest.raises(Exception) as e:
+        use('postgres', 'test')
+    assert "The module test does not belong to the group postgres." in str(e.value)
+
