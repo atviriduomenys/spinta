52710ec51e54e2365d0e03f3f43b618e2e02b929
adp <adp@atea.lt>
2023-07-14 08:31:26 +0300

    78 fixed couple of imports in api.py file

--- spinta/api.py
+++ spinta/api.py
@@ -22,17 +22,17 @@ from spinta.utils.response import create_http_response
 from spinta.accesslog import create_accesslog
 from spinta.exceptions import NoAuthServer
 
-Starlette = use('http', 'starlette.applications', 'Starlette')
-HTTPException = use('http', 'starlette.exceptions', 'HTTPException')
-JSONResponse = use('http', 'starlette.responses', 'JSONResponse')
+Starlette = use('http', 'starlette.applications', 'Starlette').Starlette
+HTTPException = use('http', 'starlette.exceptions', 'HTTPException').HTTPException
+JSONResponse = use('http', 'starlette.responses', 'JSONResponse').JSONResponse
 Request = use('http', 'starlette.requests', 'Request').Request
-Response = use('http', 'starlette.responses', 'Response')
+Response = use('http', 'starlette.responses', 'Response').Response
 RedirectResponse = use('http', 'starlette.responses', 'RedirectResponse')
 Jinja2Templates = use('http', 'starlette.templating', 'Jinja2Templates').Jinja2Templates
-StaticFiles = use('http', 'starlette.staticfiles', 'StaticFiles')
-Route = use('http', 'starlette.routing', 'Route')
-Mount = use('http', 'starlette.routing', 'Mount')
-Middleware = use('http', 'starlette.middleware', 'Middleware')
+StaticFiles = use('http', 'starlette.staticfiles', 'StaticFiles').StaticFiles
+Route = use('http', 'starlette.routing', 'Route').Route
+Mount = use('http', 'starlette.routing', 'Mount').Mount
+Middleware = use('http', 'starlette.middleware', 'Middleware').Middleware
 
 log = logging.getLogger(__name__)
 
