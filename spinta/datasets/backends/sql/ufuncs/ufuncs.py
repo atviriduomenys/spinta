d9b4b8c301469b488d70f1b91e5ece512607bb36
Justinas Kenstavicius <adp@atea.lt>
2023-08-04 08:53:38 +0300

    476 Fixed connect for backwards compatability

--- spinta/datasets/backends/sql/ufuncs/ufuncs.py
+++ spinta/datasets/backends/sql/ufuncs/ufuncs.py
@@ -1,4 +1,4 @@
-from spinta.core.ufuncs import ufunc
+from spinta.core.ufuncs import ufunc, Bind
 from spinta.datasets.backends.sql.ufuncs.components import Engine
 from spinta.datasets.backends.sql.ufuncs.components import SqlResource
 from spinta.exceptions import UnknownBind
@@ -10,3 +10,16 @@ def connect(env: SqlResource, **kwargs) -> Engine:
         if key not in ("schema", "encoding"):
             raise UnknownBind(name=key)
     return Engine(env.dsn, **kwargs)
+
+
+@ufunc.resolver(SqlResource, Bind)
+def connect(env: SqlResource, bind: Bind, **kwargs) -> Engine:
+    if bind.name == 'self':
+        return env.call('connect', env.dsn, **kwargs)
+    else:
+        raise UnknownBind(name=bind.name)
+
+
+@ufunc.resolver(SqlResource, str)
+def connect(env: SqlResource, dsn: str, **kwargs) -> Engine:
+    return Engine(dsn, **kwargs)
