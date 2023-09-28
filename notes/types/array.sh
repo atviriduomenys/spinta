fce2702e62c438d7f39df9c17fcabb381c900da2
Mantas <sirexas@gmail.com>
2023-09-28 10:17:48 +0300

    Testing notes

--- notes/types/array.sh
+++ notes/types/array.sh
@@ -13,7 +13,6 @@ $DATASET                    |         |          |
                             |         |          |
   |   |   | Language        |         |          |
   |   |   |   | name        | string  |          | open
-  |   |   |   | countries[] | backref | Country  | open
                             |         |          |
   |   |   | Country         |         |          |
   |   |   |   | name        | string  |          | open
@@ -27,68 +26,20 @@ poetry run spinta show
 # notes/spinta/server.sh    Run migrations
 
 psql -h localhost -p 54321 -U admin spinta -c '\dt "'$DATASET'"*'
-#|  Schema |                  Name                  | Type  | Owner 
-#| --------+----------------------------------------+-------+-------
-#|  public | types/array/Country                    | table | admin
-#|  public | types/array/Country/:changelog         | table | admin
-#|  public | types/array/Language                   | table | admin
-#|  public | types/array/Language/:changelog        | table | admin
-#|  public | types/array/Language/:list/countries[] | table | admin
-psql -h localhost -p 54321 -U admin spinta -c '\d "'$DATASET'/Country"'
-#|      Column      |            Type             | Collation | Nullable | Default 
-#| -----------------+-----------------------------+-----------+----------+---------
-#|  name            | text                        |           |          | 
-#| Referenced by:
-#|     TABLE "types/array/Language/:list/countries[]"
-#|       CONSTRAINT "types/array/Language/:list/countries[]_countries[]._id_fkey"
-#|         FOREIGN KEY ("countries[]._id")
-#|           REFERENCES "types/array/Country"(_id) ON DELETE CASCADE
-psql -h localhost -p 54321 -U admin spinta -c '\d "'$DATASET'/Language/:list/countries[]"'
-#|       Table "public.types/array/Language/:list/countries[]"
-#|           Column          | Type | Collation | Nullable | Default 
-#| --------------------------+------+-----------+----------+---------
-#|  _txn                     | uuid |           |          | 
-#|  types/array/Language._id | uuid |           |          | 
-#|  countries[]._id          | uuid |           |          | 
-#| Foreign-key constraints:
-#|     "types/array/Language/:list/countr_types/array/Language._id_fkey"
-#|       FOREIGN KEY ("types/array/Language._id")
-#|         REFERENCES "types/array/Language"(_id)
-#|         ON DELETE CASCADE
-#|     "types/array/Language/:list/countries[]_countries[]._id_fkey"
-#|       FOREIGN KEY ("countries[]._id")
-#|         REFERENCES "types/array/Country"(_id)
-#|         ON DELETE CASCADE
+#|                     List of relations
+#|  Schema |              Name               | Type  | Owner 
+#| --------+---------------------------------+-------+-------
+#|  public | types/array/Country             | table | admin
+#|  public | types/array/Country/:changelog  | table | admin
+#|  public | types/array/Language            | table | admin
+#|  public | types/array/Language/:changelog | table | admin
+# FIXME: for some reason there si no Language/:list/languages table
 
-# notes/spinta/server.sh    Add client
 # notes/spinta/server.sh    Run server
 # notes/spinta/client.sh    Configure client
 
 http GET "$SERVER/$DATASET/Language?format(ascii)"
-#| HTTP/1.1 404 Not Found
-#| 
-#| {
-#|     "errors": [
-#|         {
-#|             "code": "PropertyNotFound",
-#|             "context": {
-#|                 "attribute": "",
-#|                 "component": "spinta.types.datatype.BackRef",
-#|                 "dataset": "types/array",
-#|                 "entity": "",
-#|                 "manifest": "default",
-#|                 "model": "types/array/Language",
-#|                 "property": "countries[]",
-#|                 "schema": "4",
-#|                 "type": "backref"
-#|             },
-#|             "message": "Property 'countries[]' not found.",
-#|             "template": "Property {property!r} not found.",
-#|             "type": "type"
-#|         }
-#|     ]
-#| }
-
+#| HTTP/1.1 200 OK
 
 LTL=c8e4cd60-0b15-4b23-a691-09cdf2ebd9c0
 http POST "$SERVER/$DATASET/Language" $AUTH _id=$LTL name=Lithuanian
@@ -136,140 +87,64 @@ EOF
 #|     ]
 #| }
 # FIXME: This should work.
-
-
 tail -80 $BASEDIR/spinta.log
+#| ERROR: Error: Multiple errors:
+#|  - Unknown property 'languages'.
+#|      Context:
+#|        component: spinta.components.Model
+#|        manifest: default
+#|        schema: 7
+#|        dataset: types/array
+#|        model: types/array/Country
+#|        entity: 
+#|        property: languages
 #| Traceback (most recent call last):
-#|   File "spinta/api.py", line 94, in homepage
+#|   File "spinta/api.py", line 95, in homepage
 #|     return await create_http_response(context, params, request)
-#|   File "spinta/utils/response.py", line 201, in create_http_response
+#|            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#|   File "spinta/utils/response.py", line 210, in create_http_response
 #|     return await commands.push(
-#|   File "spinta/commands/write.py", line 100, in push
+#|            ^^^^^^^^^^^^^^^^^^^^
+#|   File "spinta/commands/write.py", line 102, in push
 #|     status_code, response = await simple_response(
-#|   File "spinta/commands/write.py", line 1122, in simple_response
+#|                             ^^^^^^^^^^^^^^^^^^^^^^
+#|   File "spinta/commands/write.py", line 1184, in simple_response
 #|     results = await alist(aslice(results, 2))
+#|               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #|   File "spinta/utils/aiotools.py", line 51, in alist
 #|     return [x async for x in it]
+#|            ^^^^^^^^^^^^^^^^^^^^^
 #|   File "spinta/utils/aiotools.py", line 51, in <listcomp>
 #|     return [x async for x in it]
+#|            ^^^^^^^^^^^^^^^^^^^^^
 #|   File "spinta/utils/aiotools.py", line 62, in aslice
 #|     async for x in it:
-#|   File "spinta/accesslog/__init__.py", line 174, in log_async_response
-#|     async for row in rows:
-#|   File "spinta/commands/write.py", line 185, in push_stream
+#|   File "spinta/commands/write.py", line 187, in push_stream
 #|     async for data in dstream:
 #|   File "spinta/backends/postgresql/commands/changes.py", line 25, in create_changelog_entry
 #|     async for data in dstream:
-#|   File "spinta/backends/postgresql/commands/write.py", line 23, in insert
+#|   File "spinta/backends/postgresql/commands/write.py", line 31, in insert
 #|     async for data in dstream:
-#|   File "spinta/commands/write.py", line 845, in prepare_data_for_write
+#|   File "spinta/commands/write.py", line 861, in prepare_data_for_write
 #|     async for data in dstream:
-#|   File "spinta/commands/write.py", line 646, in prepare_patch
+#|   File "spinta/commands/write.py", line 648, in prepare_patch
 #|     async for data in dstream:
-#|   File "spinta/commands/write.py", line 616, in validate_data
+#|   File "spinta/commands/write.py", line 618, in validate_data
 #|     async for data in dstream:
-#|   File "spinta/commands/write.py", line 519, in read_existing_data
+#|   File "spinta/commands/write.py", line 521, in read_existing_data
 #|     async for data in dstream:
-#|   File "spinta/commands/write.py", line 507, in prepare_data
+#|   File "spinta/commands/write.py", line 509, in prepare_data
 #|     data.given = commands.load(context, data.model, data.payload)
-#|   File "spinta/types/model.py", line 336, in load
-#|     exceptions.FieldNotInResource(model, property=prop)
+#|                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#|   File "spinta/types/model.py", line 370, in load
+#|     raise exceptions.MultipleErrors(
 #| spinta.exceptions.MultipleErrors: Multiple errors:
 #|  - Unknown property 'languages'.
 #|      Context:
 #|        component: spinta.components.Model
 #|        manifest: default
-#|        schema: 8
+#|        schema: 7
 #|        dataset: types/array
 #|        model: types/array/Country
 #|        entity: 
 #|        property: languages
-
-
-http GET "$SERVER/$DATASET/Language"
-#| HTTP/1.1 404 Not Found
-#| 
-#| {
-#|     "errors": [
-#|         {
-#|             "code": "PropertyNotFound",
-#|             "context": {
-#|                 "attribute": "",
-#|                 "component": "spinta.types.datatype.BackRef",
-#|                 "dataset": "types/array",
-#|                 "entity": "",
-#|                 "manifest": "default",
-#|                 "model": "types/array/Language",
-#|                 "property": "countries[]",
-#|                 "schema": "4",
-#|                 "type": "backref"
-#|             },
-#|             "message": "Property 'countries[]' not found.",
-#|             "template": "Property {property!r} not found.",
-#|             "type": "type"
-#|         }
-#|     ]
-#| }
-# TODO: https://github.com/atviriduomenys/spinta/issues/96
-# FIXME: Do not include `backref` properties in the result, unless explicityly
-#        selected.
-
-
-tail -80 $BASEDIR/spinta.log
-#| Traceback (most recent call last):
-#|   File "spinta/api.py", line 94, in homepage
-#|     return await create_http_response(context, params, request)
-#|   File "spinta/utils/response.py", line 153, in create_http_response
-#|     return await commands.getall(
-#|   File "spinta/commands/read.py", line 121, in getall
-#|     return render(context, request, model, params, rows, action=action)
-#|   File "spinta/renderer.py", line 23, in render
-#|     return commands.render(
-#|   File "spinta/formats/json/commands.py", line 25, in render
-#|     return _render(
-#|   File "spinta/formats/json/commands.py", line 52, in _render
-#|     aiter(peek_and_stream(fmt(data))),
-#|   File "spinta/utils/response.py", line 223, in peek_and_stream
-#|     peek = list(itertools.islice(stream, 2))
-#|   File "spinta/formats/json/components.py", line 16, in __call__
-#|     for i, row in enumerate(data):
-#|   File "spinta/accesslog/__init__.py", line 162, in log_response
-#|     for row in rows:
-#|   File "spinta/commands/read.py", line 106, in <genexpr>
-#|     rows = (
-#|   File "spinta/backends/postgresql/commands/read.py", line 51, in getall
-#|     expr = env.resolve(query)
-#|   File "spinta/core/ufuncs.py", line 242, in resolve
-#|     return ufunc(self, expr)
-#|   File "spinta/backends/postgresql/commands/query.py", line 243, in select
-#|     env.call('select', Star())
-#|   File "spinta/backends/postgresql/commands/query.py", line 255, in select
-#|     env.select[prop.place] = env.call('select', prop.dtype)
-#|   File "spinta/backends/postgresql/commands/query.py", line 284, in select
-#|     column = env.backend.get_column(table, dtype.prop, select=True)
-#|   File "spinta/backends/postgresql/components.py", line 119, in get_column
-#|     raise PropertyNotFound(prop.dtype)
-#| spinta.exceptions.PropertyNotFound: Property 'countries[]' not found.
-#|   Context:
-#|     component: spinta.types.datatype.BackRef
-#|     manifest: default
-#|     schema: 4
-#|     dataset: types/array
-#|     model: types/array/Language
-#|     entity: 
-#|     property: countries[]
-#|     attribute: 
-#|     type: backref
-
-
-http GET "$SERVER/$DATASET/Country"
-#| HTTP/1.1 500 Internal Server Error
-#| 
-#| {
-#|     "errors": [
-#|         {
-#|             "code": "KeyError",
-#|             "message": "'languages[]._id'"
-#|         }
-#|     ]
-#| }
