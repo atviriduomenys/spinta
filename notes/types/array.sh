acc03bf1a2a23d87f1847c8e72cc958acb1f14bc
Mantas <sirexas@gmail.com>
2023-10-04 17:54:22 +0300

    Testing notes

--- notes/types/array.sh
+++ notes/types/array.sh
@@ -22,18 +22,15 @@ poetry run spinta copy $BASEDIR/manifest.txt -o $BASEDIR/manifest.csv
 cat $BASEDIR/manifest.csv
 poetry run spinta show
 
-# notes/spinta/server.sh    Check configuration
 # notes/spinta/server.sh    Run migrations
 
 psql -h localhost -p 54321 -U admin spinta -c '\dt "'$DATASET'"*'
-#|                     List of relations
-#|  Schema |              Name               | Type  | Owner 
-#| --------+---------------------------------+-------+-------
-#|  public | types/array/Country             | table | admin
-#|  public | types/array/Country/:changelog  | table | admin
-#|  public | types/array/Language            | table | admin
-#|  public | types/array/Language/:changelog | table | admin
-# FIXME: for some reason there si no Language/:list/languages table
+#|                       List of relations
+#|  Schema |                Name                 | Type  | Owner 
+#| --------+-------------------------------------+-------+-------
+#|  public | types/array/Country                 | table | admin
+#|  public | types/array/Country/:list/languages | table | admin
+#|  public | types/array/Language                | table | admin
 
 # notes/spinta/server.sh    Run server
 # notes/spinta/client.sh    Configure client
@@ -41,30 +38,151 @@ psql -h localhost -p 54321 -U admin spinta -c '\dt "'$DATASET'"*'
 http GET "$SERVER/$DATASET/Language?format(ascii)"
 #| HTTP/1.1 200 OK
 
-LTL=c8e4cd60-0b15-4b23-a691-09cdf2ebd9c0
-http POST "$SERVER/$DATASET/Language" $AUTH _id=$LTL name=Lithuanian
+LIT=c8e4cd60-0b15-4b23-a691-09cdf2ebd9c0
+http POST "$SERVER/$DATASET/Language" $AUTH _id=$LIT name=Lithuanian
 #| HTTP/1.1 201 Created
 #| 
 #| {
 #|     "_id": "c8e4cd60-0b15-4b23-a691-09cdf2ebd9c0",
-#|     "_revision": "4a825ed1-0936-4668-ab80-c3bd424ec3ef",
+#|     "_revision": "bc3c4024-fb94-47de-beeb-1b11111b39d9",
 #|     "_type": "types/array/Language",
-#|     "countries[]": null,
 #|     "name": "Lithuanian"
 #| }
-REV=$(http GET "$SERVER/$DATASET/Language/$LTL" | jq -r '._revision')
 
 
-LTC=d73306fb-4ee5-483d-9bad-d86f98e1869c
+LT=d73306fb-4ee5-483d-9bad-d86f98e1869c
 http POST "$SERVER/$DATASET/Country" $AUTH <<EOF
 {
-    "_id": "$LTC",
+    "_id": "$LT",
     "name": "Lithuania",
     "languages": [
-        {"_id": "$LTL"}
+        {"_id": "$LIT"}
     ]
 }
 EOF
+#| HTTP/1.1 201 Created
+#| 
+#| {
+#|     "_id": "d73306fb-4ee5-483d-9bad-d86f98e1869c",
+#|     "_revision": "fa4dd99f-a782-4d13-9480-0a258a9d0790",
+#|     "_type": "types/array/Country",
+#|     "languages": [
+#|         {
+#|             "_id": "c8e4cd60-0b15-4b23-a691-09cdf2ebd9c0"
+#|         }
+#|     ],
+#|     "name": "Lithuania"
+#| }
+
+psql -h localhost -p 54321 -U admin spinta -c 'SELECT * FROM "'$DATASET'/Country/:list/languages";'
+#|                  _txn                 |                 _rid                 | languages._id 
+#| --------------------------------------+--------------------------------------+---------------
+#|  d16de594-ba6a-485d-b411-7289ff286757 | d73306fb-4ee5-483d-9bad-d86f98e1869c | 
+
+
+SGS=328cfde7-2365-4625-bf72-e9b3038e12ac
+http POST "$SERVER/$DATASET/Language" $AUTH _id=$SGS name=Samogitian
+
+REV=$(http GET "$SERVER/$DATASET/Country/$LT" | jq -r '._revision')
+http PATCH "$SERVER/$DATASET/Country/$LT" $AUTH <<EOF
+{
+    "_revision": "$REV",
+    "languages": [
+        {"_id": "$LIT"},
+        {"_id": "$SGS"}
+    ]
+}
+EOF
+#| HTTP/1.1 200 OK
+#| 
+#| {
+#|     "_id": "d73306fb-4ee5-483d-9bad-d86f98e1869c",
+#|     "_revision": "7ac9981c-2158-4ac9-8f8e-7849ee194ec8",
+#|     "_type": "types/array/Country",
+#|     "languages": [
+#|         {
+#|             "_id": "c8e4cd60-0b15-4b23-a691-09cdf2ebd9c0"
+#|         },
+#|         {
+#|             "_id": "328cfde7-2365-4625-bf72-e9b3038e12ac"
+#|         }
+#|     ]
+#| }
+
+http GET "$SERVER/$DATASET/Country"
+#| {
+#|     "_data": [
+#|         {
+#|             "_id": "d73306fb-4ee5-483d-9bad-d86f98e1869c",
+#|             "_page": "WyJkNzMzMDZmYi00ZWU1LTQ4M2QtOWJhZC1kODZmOThlMTg2OWMiXQ==",
+#|             "_revision": "7ac9981c-2158-4ac9-8f8e-7849ee194ec8",
+#|             "_type": "types/array/Country",
+#|             "name": "Lithuania"
+# FIXME: Here I expect to see:
+#        "languages": []
+#|         }
+#|     ]
+#| }
+
+
+http GET "$SERVER/$DATASET/Country/languages"
+#| HTTP/1.1 404 Not Found
+#| 
+#| {
+#|     "errors": [
+#|         {
+#|             "code": "ModelNotFound",
+#|             "context": {
+#|                 "model": "types/array/Country/languages"
+#|             },
+#|             "message": "Model 'types/array/Country/languages' not found.",
+#|             "template": "Model {model!r} not found.",
+#|             "type": "system"
+#|         }
+#|     ]
+#| }
+# TODO: This should work.
+
+
+http GET "$SERVER/$DATASET/Country/$LT/languages"
+#| HTTP/1.1 400 Bad Request
+#| 
+#| {
+#|     "errors": [
+#|         {
+#|             "code": "UnavailableSubresource",
+#|             "context": {
+#|                 "prop": "languages",
+#|                 "prop_type": "array"
+#|             },
+#|             "message": "Subresources only of type Object and File are accessible",
+#|             "template": "Subresources only of type Object and File are accessible",
+#|             "type": "system"
+#|         }
+#|     ]
+#| }
+# TODO: This should work.
+
+
+http GET "$SERVER/$DATASET/Country?expand()"
+#| {
+#|     "_data": [
+#|         {
+#|             "_id": "d73306fb-4ee5-483d-9bad-d86f98e1869c",
+#|             "_page": "WyJkNzMzMDZmYi00ZWU1LTQ4M2QtOWJhZC1kODZmOThlMTg2OWMiXQ==",
+#|             "_revision": "7ac9981c-2158-4ac9-8f8e-7849ee194ec8",
+#|             "_type": "types/array/Country",
+#|             "name": "Lithuania"
+# FIXME: Here I expect to see:
+#        "languages": [
+#            {"_id": "$LIT"},
+#            {"_id": "$SGS"}
+#        ]
+#|         }
+#|     ]
+#| }
+
+http GET "$SERVER/$DATASET/Country?expand(languages)"
 #| HTTP/1.1 400 Bad Request
 #| 
 #| {
@@ -78,7 +196,7 @@ EOF
 #|                 "manifest": "default",
 #|                 "model": "types/array/Country",
 #|                 "property": "languages",
-#|                 "schema": "8"
+#|                 "schema": "7"
 #|             },
 #|             "message": "Unknown property 'languages'.",
 #|             "template": "Unknown property {property!r}.",
@@ -86,65 +204,93 @@ EOF
 #|         }
 #|     ]
 #| }
-# FIXME: This should work.
-tail -80 $BASEDIR/spinta.log
-#| ERROR: Error: Multiple errors:
-#|  - Unknown property 'languages'.
-#|      Context:
-#|        component: spinta.components.Model
-#|        manifest: default
-#|        schema: 7
-#|        dataset: types/array
-#|        model: types/array/Country
-#|        entity: 
-#|        property: languages
-#| Traceback (most recent call last):
-#|   File "spinta/api.py", line 95, in homepage
-#|     return await create_http_response(context, params, request)
-#|            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#|   File "spinta/utils/response.py", line 210, in create_http_response
-#|     return await commands.push(
-#|            ^^^^^^^^^^^^^^^^^^^^
-#|   File "spinta/commands/write.py", line 102, in push
-#|     status_code, response = await simple_response(
-#|                             ^^^^^^^^^^^^^^^^^^^^^^
-#|   File "spinta/commands/write.py", line 1184, in simple_response
-#|     results = await alist(aslice(results, 2))
-#|               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#|   File "spinta/utils/aiotools.py", line 51, in alist
-#|     return [x async for x in it]
-#|            ^^^^^^^^^^^^^^^^^^^^^
-#|   File "spinta/utils/aiotools.py", line 51, in <listcomp>
-#|     return [x async for x in it]
-#|            ^^^^^^^^^^^^^^^^^^^^^
-#|   File "spinta/utils/aiotools.py", line 62, in aslice
-#|     async for x in it:
-#|   File "spinta/commands/write.py", line 187, in push_stream
-#|     async for data in dstream:
-#|   File "spinta/backends/postgresql/commands/changes.py", line 25, in create_changelog_entry
-#|     async for data in dstream:
-#|   File "spinta/backends/postgresql/commands/write.py", line 31, in insert
-#|     async for data in dstream:
-#|   File "spinta/commands/write.py", line 861, in prepare_data_for_write
-#|     async for data in dstream:
-#|   File "spinta/commands/write.py", line 648, in prepare_patch
-#|     async for data in dstream:
-#|   File "spinta/commands/write.py", line 618, in validate_data
-#|     async for data in dstream:
-#|   File "spinta/commands/write.py", line 521, in read_existing_data
-#|     async for data in dstream:
-#|   File "spinta/commands/write.py", line 509, in prepare_data
-#|     data.given = commands.load(context, data.model, data.payload)
-#|                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#|   File "spinta/types/model.py", line 370, in load
-#|     raise exceptions.MultipleErrors(
-#| spinta.exceptions.MultipleErrors: Multiple errors:
-#|  - Unknown property 'languages'.
-#|      Context:
-#|        component: spinta.components.Model
-#|        manifest: default
-#|        schema: 7
-#|        dataset: types/array
-#|        model: types/array/Country
-#|        entity: 
-#|        property: languages
+# TODO: `expand(languages)` should work.
+
+
+http GET "$SERVER/$DATASET/Country?format(ascii)"
+#| name     
+#| ---------
+#| Lithuania
+# FIXME: `languages[]` sould be shown here.
+
+
+http GET "$SERVER/$DATASET/Country?expand()&format(rdf)"
+#| <?xml version="1.0" encoding="UTF-8"?>
+#| <Rdf xml:base="http://localhost:8000/">
+#|   <Description about="/types/array/Country/d73306fb-4ee5-483d-9bad-d86f98e1869c" type="types/array/Country" version="7ac9981c-2158-4ac9-8f8e-7849ee194ec8">
+#|     <_page>WyJkNzMzMDZmYi00ZWU1LTQ4M2QtOWJhZC1kODZmOThlMTg2OWMiXQ==</_page>
+#|     <name>Lithuania</name>
+# FIXME: Here I expect to see:
+#        <languages rdf:resource="/types/array/Language/$LIT" />
+#        <languages rdf:resource="/types/array/Language/$SGS" />
+#|   </Description>
+#| </Rdf>
+
+
+http GET "$SERVER/$DATASET/Country/:changes"
+#| {
+#|     "_data": [
+#|         {
+#|             "_cid": 1,
+#|             "_created": "2023-10-04T12:06:16.907090",
+#|             "_id": "d73306fb-4ee5-483d-9bad-d86f98e1869c",
+#|             "_op": "insert",
+#|             "_revision": "fa4dd99f-a782-4d13-9480-0a258a9d0790",
+#|             "_txn": "95a21aec-cd77-42fa-b0f1-dde4c00e6292",
+#|             "name": "Lithuania"
+# FIXME: Languages should be shown here:
+#          "languages": [
+#              {"_id": "$LIT"}
+#          ]
+#|         },
+#|         {
+#|             "_cid": 2,
+#|             "_created": "2023-10-04T12:12:08.114911",
+#|             "_id": "d73306fb-4ee5-483d-9bad-d86f98e1869c",
+#|             "_op": "patch",
+#|             "_revision": "7ac9981c-2158-4ac9-8f8e-7849ee194ec8",
+#|             "_txn": "bc01d1d0-8611-4f45-a1e7-6665a10b26ff"
+# FIXME: Languages should be shown here:
+#          "languages": [
+#              {"_id": "$LIT"},
+#              {"_id": "$SGS"}
+#          ]
+#|         }
+#|     ]
+#| }
+
+http DELETE "$SERVER/$DATASET/Country/$LT" $AUTH
+#| HTTP/1.1 204 No Content
+
+http GET "$SERVER/$DATASET/Country"
+#| {"_data": []}
+
+http POST "$SERVER/$DATASET/Country" $AUTH <<EOF
+{
+    "_id": "$LT",
+    "name": "Lithuania",
+    "languages": [
+        {"_id": "$LIT"},
+        {"_id": "$SGS"}
+    ]
+}
+EOF
+
+http GET "$SERVER/$DATASET/Country?format(ascii)"
+
+http DELETE "$SERVER/$DATASET/Country/:wipe" $AUTH
+#| {"wiped": true}
+
+http GET "$SERVER/$DATASET/Country?format(ascii)"
+
+
+psql -h localhost -p 54321 -U admin spinta -c 'SELECT * FROM "'$DATASET'/Country/:list/languages";'
+#|  _txn | _rid | languages._id 
+#| ------+------+---------------
+#| (0 rows)
+
+
+http DELETE "$SERVER/$DATASET/:wipe" $AUTH
+
+
+
