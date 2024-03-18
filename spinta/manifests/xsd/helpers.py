b142cbe02af2c9c8d74a301b25bbaf327f61acd8
Karina Klinkevičiūtė <karina.klinkeviciute@ivpk.lt>
2024-03-18 19:50:21 +0200

    handling of an element as a property

--- spinta/manifests/xsd/helpers.py
+++ spinta/manifests/xsd/helpers.py
@@ -1,5 +1,4 @@
-import xmltodict
-from lxml.etree import _ElementTree
+from lxml.etree import _ElementTree, _Element
 
 from spinta.components import Context
 from lxml import etree, objectify
@@ -10,6 +9,7 @@ from pprint import pprint
 # mapping of XSD datatypes to DSA datatypes
 # XSD datatypes: https://www.w3.org/TR/xmlschema11-2/#built-in-datatypes
 # DSA datatypes: https://atviriduomenys.readthedocs.io/dsa/duomenu-tipai.html#duomenu-tipai
+# todo finish mapping and make sure all things are mapped correctly
 DATATYPES_MAPPING = {
     "string": "text",
     "boolean": "boolean",
@@ -70,7 +70,7 @@ DATATYPES_MAPPING = {
 }
 
 
-def get_description(element: etree.Element) -> str:
+def _get_description(element: etree.Element) -> str:
     annotation = element.xpath(f'./*[local-name() = "annotation"]')
     if annotation:
         documentation = annotation[0].xpath(f'./*[local-name() = "documentation"]')
@@ -79,46 +79,77 @@ def get_description(element: etree.Element) -> str:
     return ""
 
 
-def attributes_to_properties(element: etree.Element):
+def _node_to_partial_property(node: etree.Element) -> tuple[str, dict]:
+    """Node can be either element or attribute. only process things common to attributes and elements"""
+    prop = dict()
+    prop["description"] = _get_description(node)
+    property_name = node.get("name").lower()
+    property_id = property_name
+    prop["external"] = {"name": property_name}
+
+    property_type = node.get("type")
+    if not property_type:
+        # this is a self defined simple type, so we take it's base as type
+        restrictions = node.xpath(f'./*[local-name() = "simpleType"]/*[local-name() = "restriction"]')
+        if restrictions:
+            property_type = restrictions[0].get("base", "")
+        else:
+            property_type = ""
+    # getting rid of the prefix
+    if ":" in property_type:
+        property_type = property_type.split(":")[1]
+    prop["type"] = DATATYPES_MAPPING[property_type]
+
+    return property_id, prop
+
+
+def _element_to_property(element: etree.Element) -> tuple[str, dict]:
+    """
+    Receives an element and returns a tuple containing the id of the property and the property itself
+    Elements can be properties in those cases when they have only description (annotation/documentation)
+    """
+    property_id, prop = _node_to_partial_property(element)
+
+    # if maxOccurs > 1, then it's a list. specific to elements.
+    max_occurs = element.get("maxOccurs", 1)
+    if max_occurs > 1:
+        prop["external"]["name"] += "[]"
+
+    # specific to elements
+    min_occurs = element.get("minOccurs", 1)
+    if min_occurs > 0:
+        prop["required"] = True
+    else:
+        prop["required"] = False
+
+    prop["source"] = f"{prop["external"]["name"].upper()}"
+
+    return property_id, prop
+
+
+def _attributes_to_properties(element: etree.Element) -> dict:
     properties = {}
     attributes = element.xpath(f'./*[local-name() = "attribute"]')
     for attribute in attributes:
 
-        # property id
-        property_id = attribute.get("name").lower()
-        properties[property_id] = {}
+        property_id, prop = _node_to_partial_property(attribute)
 
-        # property type
-        property_type = attribute.get("type", "")
-        if not property_type:
+        # property source
+        prop["source"] = f"@{prop["external"]["name"]}"
 
-            # this is a self defined simple type, so we take it's base as type
-            restrictions = attribute.xpath(f'./*[local-name() = "simpleType"]/*[local-name() = "restriction"]')
-            if restrictions:
-                property_type = restrictions[0].get("base")
-            else:
-                property_type = ""
-
-        # getting rid of the prefix
-        if ":" in property_type:
-            property_type = property_type.split(":")[1]
-
-        property_type = DATATYPES_MAPPING[property_type]
-        properties[property_id]["type"] = property_type
-
-        # property required or not
+        # property required or not. For attributes only.
         use = attribute.get("use")
         if use == "required":
             required = True
         else:
             required = False
-        properties[property_id]["required"] = required
-        properties[property_id]["description"] = get_description(attribute)
+        prop["required"] = required
+        properties[property_id] = prop
 
     return properties
 
 
-def get_properties(element: _ElementTree) -> dict:
+def _get_properties(element: _ElementTree, source_path: str) -> dict:
     """
     XSD attributes will get turned into properties
 
@@ -141,40 +172,57 @@ def get_properties(element: _ElementTree) -> dict:
     """
     properties = {}
 
-    attributes = attributes_to_properties(element)
+    attributes = _attributes_to_properties(element)
     properties.update(attributes)
+    # todo add sequences and choices
 
+    """
+    source: https://stackoverflow.com/questions/36286056/the-difference-between-all-sequence-choice-and-group-in-xsd
+    When to use xsd:all, xsd:sequence, xsd:choice, or xsd:group:
+
+    Use xsd:all when all child elements must be present, independent of order.
+    Use xsd:sequence when child elements must be present per their occurrence constraints and order does matters.
+    Use xsd:choice when one of the child element must be present.
+    Use xsd:group as a way to wrap any of the above in order to name and reuse in multiple locations within an XSD.
+    Note that occurrence constraints can appear on xsd:all, xsd:sequence, or xsd:choice in addition to the 
+    child elements to achieve various cardinality effects.
+
+    For example, if minOccurs="0" were added to xsd:element children of xsd:all, element order would be insignificant,
+    but not all child elements would have to be present:
+    """
     return properties
 
 
-def parse_model(model: _ElementTree) -> dict:
+def parse_element(element: _Element, models: list, source_path: str = "") -> dict:
+    """
+    Parses an element. If it is a complete model, it will be added to the models list.
     """
 
-    """
+    source_path = f"/{source_path}{element.get('name')}"
+
     parsed_model = {
         "type": "model",
         "description": "",
-        "name": model.get("name").capitalize(),
-        "title": model.get("name").capitalize(),
+        "name": element.get("name").capitalize(),
+        "title": element.get("name").capitalize(),
+        "source": source_path,
     }
 
-    for element in model:
+    for element in element:
         # print(element.prefix)
         # element.tag = etree.QName(element).localname
-        parsed_model["description"] = get_description(model)
-        parsed_model["properties"] = get_properties(element)
+        parsed_model["description"] = _get_description(element)
+        parsed_model["properties"] = _get_properties(element, source_path)
 
         # print(element.xpath("xs:documentation", namespaces={'xs': 'http://www.w3.org/2001/XMLSchema'})[0].text)
 
         # print(etree.tostring(element, encoding="utf8"))
-    pprint(parsed_model)
     return parsed_model
 
 
 def get_external_info(path: str, document: _ElementTree) -> dict:
     # todo finish this
     pass
-
 
 
 def read_schema(context: Context, path: str, prepare: str = None, dataset_name: str = '') -> dict:
@@ -192,18 +240,32 @@ def read_schema(context: Context, path: str, prepare: str = None, dataset_name: 
        and assign the parsed results to properties of the model
     3. If this element has only annotation, we create a special Resource model and
        add this element as property to that Resource model
+    4. If this element has only sequence with references, and none of its references has
+       minOccurs set to 1, then we don't create model out of it but go deeper into all refs
+    5. If this element has only sequence with, and some of the references have minOccurs set to 1,
+    then we create a reference which is like this: ELEMENT_NAME@attribute
+    6. Choices are handled similar to sequences
 
-    What to do with sequences?
-    What to do with choices?
+
+    We will build a list of models, and another list, that has "used" elements,
+    that have already been used in models either as part of the path or as part of the property.
+    (another option was to make a generator for parsing models, and going deeper, but that would
+    be more complex when returning models. although this option is also possible, but it can
+    be reworked into this from an option with a list)
+
+    If some model has only references in the sequence, we need to also add those as models,
+    mark their type as backref, and set as private. In this case, we add them at the moment we meet them
+    as refs, because this way we will know that they are backrefs.
+
+    Element can be as a choice in more than one other element.
 
     """
     document = etree.parse(urlopen(path))
     print(type(document))
 
-    # todo find out if we could make this work:
-    # etree.cleanup_namespaces(document)
     objectify.deannotate(document, cleanup_namespaces=True)
-    document = document.getroot()
+    root = document.getroot()
+    print(type(root))
 
     external_info = get_external_info(path, document)
 
@@ -213,19 +275,30 @@ def read_schema(context: Context, path: str, prepare: str = None, dataset_name: 
         "properties": [],
         "name": "Resource",
         "external": external_info,
+        "uri": "http://www.w3.org/2000/01/rdf-schema#Resource",
     }
-
-    for model in document:
+    models = []
+    for element in root:
 
         # first we need to check if this model has complexType.
         # If it has, we create a separate model.
         # If it doesn't have, we add a special model Resource and add this element as a property to it
         # model.tag = etree.QName(model).localname
-        if model.xpath(f'./*[local-name() = "complexType"]'):
-            parsed_model = parse_model(model)
-            parsed_model["external"] = external_info
+        print(element.xpath("*"))
+        print(element.attrib)
+
+        # todo complexContent is also an option. It doesn't seem to be used by RC but possible option for future
+        # todo there is also an option where complex type is on the same level as element, referenced by type
+        if element.xpath(f'./*[local-name() = "complexType"]'):
+
+            parse_element(element, models)
+
         else:
             # todo add logic for adding this as a property to the Resource model
             pass
-        # yield model
 
+    for parsed_model in models:
+        parsed_model["external"] = external_info
+        pprint(parsed_model)
+
+        # yield model
