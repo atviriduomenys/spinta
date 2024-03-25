9d76bacd66b51ad6a54d2b1bd0581f0f49cc513b
karina <karina.klinkeviciute@gmail.com>
2024-03-25 19:37:02 +0200

    references; added some tests

---
+++ tests/manifests/xsd/test_helpers.py
@@ -0,0 +1,136 @@
+from lxml import etree
+
+from spinta.manifests.xsd.helpers import _get_description, _get_property_type, _node_to_partial_property
+
+
+def test_get_description():
+    element_string = """
+    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" elementFormDefault="qualified">
+    <xs:element name="klaida" type="xs:string">
+        <xs:annotation><xs:documentation>Klaidos atveju - klaidos pranešimas</xs:documentation></xs:annotation>
+    </xs:element>
+    </xs:schema>
+    """
+
+    schema = etree.fromstring(element_string)
+    element = schema.xpath('*[local-name() = "element"]')[0]
+    result = _get_description(element)
+
+    assert result == "Klaidos atveju - klaidos pranešimas"
+
+
+def test_get_property_type():
+    element_string = """
+    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" elementFormDefault="qualified">
+    <xs:element name="klaida" type="xs:string">
+        <xs:annotation><xs:documentation>Klaidos atveju - klaidos pranešimas</xs:documentation></xs:annotation>
+    </xs:element>
+    </xs:schema>
+    """
+
+    schema = etree.fromstring(element_string)
+    element = schema.xpath('*[local-name() = "element"]')[0]
+    result = _get_property_type(element)
+
+    assert result == "string"
+
+
+def test_get_property_type_ref():
+    element_string = """
+    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" elementFormDefault="qualified">
+    <xs:element name="klientu_saraso_rezultatas">
+      <xs:complexType mixed="true">
+        <xs:sequence>
+          <xs:element ref="asmenys" minOccurs="0" maxOccurs="1" />
+        </xs:sequence>
+      </xs:complexType>
+    </xs:element>
+    </xs:schema>
+    """
+
+    schema = etree.fromstring(element_string)
+    element = schema.xpath('//*[@ref="asmenys"]', )[0]
+    print("ELEMENT:", element)
+    result = _get_property_type(element)
+
+    assert result == "ref"
+
+
+def test_get_property_type_simple_type():
+    element_string = """
+    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" elementFormDefault="qualified">
+    <xs:element name="CT_E200_FORMA">
+      <xs:annotation><xs:documentation>E200 medicininės formos pavadinimas</xs:documentation></xs:annotation>
+      <xs:simpleType>
+        <xs:restriction base="xs:string">
+          <xs:maxLength value="4"/>
+        </xs:restriction>
+      </xs:simpleType>
+    </xs:element>
+    </xs:schema>
+    """
+
+    schema = etree.fromstring(element_string)
+    element = schema.xpath('*[local-name() = "element"]')[0]
+    result = _get_property_type(element)
+
+    assert result == "string"
+
+
+def test_node_to_partial_property():
+    element_string = """
+    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" elementFormDefault="qualified">
+    <xs:element name="CT_E200_FORMA">
+      <xs:annotation><xs:documentation>E200 medicininės formos pavadinimas</xs:documentation></xs:annotation>
+      <xs:simpleType>
+        <xs:restriction base="xs:string">
+          <xs:maxLength value="4"/>
+        </xs:restriction>
+      </xs:simpleType>
+    </xs:element>
+    </xs:schema>
+    """
+
+    schema = etree.fromstring(element_string)
+    element = schema.xpath('*[local-name() = "element"]')[0]
+    result1, result2 = _node_to_partial_property(element)
+
+    assert result1 == "ct_e200_forma"
+    assert result2 == {
+        "description": "E200 medicininės formos pavadinimas",
+        "external": {
+            "name": "CT_E200_FORMA",
+        },
+        "type": "string"
+    }
+# todo test properties with refs
+
+
+def test_element_to_property():
+    element_string = """
+    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" elementFormDefault="qualified">
+    <xs:element name="CT_E200_FORMA">
+      <xs:annotation><xs:documentation>E200 medicininės formos pavadinimas</xs:documentation></xs:annotation>
+      <xs:simpleType>
+        <xs:restriction base="xs:string">
+          <xs:maxLength value="4"/>
+        </xs:restriction>
+      </xs:simpleType>
+    </xs:element>
+    </xs:schema>
+    """
+    schema = etree.fromstring(element_string)
+    element = schema.xpath('*[local-name() = "element"]')[0]
+    result1, result2 = _node_to_partial_property(element)
+    assert result1 == "ct_e200_forma"
+    assert result2 == {
+        "description": "E200 medicininės formos pavadinimas",
+        "external": {
+            "name": "CT_E200_FORMA/text()",
+        },
+        "type": "string",
+        "required": False
+    }
+
+
+# def test_get_external_info():
