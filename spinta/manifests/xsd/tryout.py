efcdca3fbbfaf549f7be80ee8563e126c2e984d4
karina <karina.klinkeviciute@gmail.com>
2024-04-03 17:59:44 +0300

    a bit more structures covered

--- spinta/manifests/xsd/tryout.py
+++ spinta/manifests/xsd/tryout.py
@@ -7,13 +7,17 @@ from spinta.manifests.xsd.helpers import read_schema
 
 # result = read_schema(context=Context("xsd"), path="https://ws.registrucentras.lt/broker/info.php?t=717&f=out")
 
-result = read_schema(context=Context("xsd"), path="/home/karina/work/ivpk/spinta/rc717.xsd")
+result = read_schema(context=Context("xsd"), path="/home/karina/work/ivpk/spinta/rc729.xsd")
 # result = read_schema(context=Context("xsd"), path="/home/karina/work/ivpk/spinta/example.xsd")
 
-# 729 -
+# 729 +
 # 717
 # 765
-# 588
+# 588 +
+# 589 +
+# 590 +
+# 591 +
+# 605 +
 # 5501
 # rc249jadis-israsas - tipai aprasomi isoriniam dokumente
 # 437
