28777de9deb239c2d755534dbbc70e662d69110d
karina <karina.klinkeviciute@gmail.com>
2024-05-03 13:40:45 +0300

    uncommented tests

--- tests/datasets/dataframe/test_ufunc.py
+++ tests/datasets/dataframe/test_ufunc.py
@@ -24,57 +24,57 @@ def fs():
     yield fs
     MemoryFileSystem.store.clear()
 
-#
-# def test_csv_tabular_seperator_default_error(rc: RawConfig, fs: AbstractFileSystem):
-#     fs.pipe('cities.csv', (
-#         'šalis;miestas\n'
-#         'lt;Vilnius\n'
-#         'lv;Ryga\n'
-#         'ee;Talin'
-#     ).encode('utf-8'))
-#
-#     context, manifest = prepare_manifest(rc, '''
-#     d | r | b | m | property | type    | ref  | source              | prepare | access
-#     example                  |         |      |                     |         |
-#       | csv                  | csv     |      | memory://cities.csv |         |
-#       |   |   | City         |         | name |                     |         |
-#       |   |   |   | name     | string  |      | miestas             |         | open
-#       |   |   |   | country  | string  |      | šalis               |         | open
-#     ''', mode=Mode.external)
-#     context.loaded = True
-#     app = create_test_client(context)
-#     app.authmodel('example/City', ['getall'])
-#
-#     resp = app.get('/example/City')
-#     assert resp.status_code == 404
-#
-#
-# def test_csv_tabular_seperator(rc: RawConfig, fs: AbstractFileSystem):
-#     fs.pipe('cities.csv', (
-#         'šalis;miestas\n'
-#         'lt;Vilnius\n'
-#         'lv;Ryga\n'
-#         'ee;Talin'
-#     ).encode('utf-8'))
-#
-#     context, manifest = prepare_manifest(rc, '''
-#     d | r | b | m | property | type    | ref  | source              | prepare          | access
-#     example                  |         |      |                     |                  |
-#       | csv                  | csv     |      | memory://cities.csv | tabular(sep:";") |
-#       |   |   | City         |         | name |                     |                  |
-#       |   |   |   | name     | string  |      | miestas             |                  | open
-#       |   |   |   | country  | string  |      | šalis               |                  | open
-#     ''', mode=Mode.external)
-#     context.loaded = True
-#     app = create_test_client(context)
-#     app.authmodel('example/City', ['getall'])
-#
-#     resp = app.get('/example/City')
-#     assert listdata(resp, sort=False) == [
-#         ('lt', 'Vilnius'),
-#         ('lv', 'Ryga'),
-#         ('ee', 'Talin'),
-#     ]
+
+def test_csv_tabular_seperator_default_error(rc: RawConfig, fs: AbstractFileSystem):
+    fs.pipe('cities.csv', (
+        'šalis;miestas\n'
+        'lt;Vilnius\n'
+        'lv;Ryga\n'
+        'ee;Talin'
+    ).encode('utf-8'))
+
+    context, manifest = prepare_manifest(rc, '''
+    d | r | b | m | property | type    | ref  | source              | prepare | access
+    example                  |         |      |                     |         |
+      | csv                  | csv     |      | memory://cities.csv |         |
+      |   |   | City         |         | name |                     |         |
+      |   |   |   | name     | string  |      | miestas             |         | open
+      |   |   |   | country  | string  |      | šalis               |         | open
+    ''', mode=Mode.external)
+    context.loaded = True
+    app = create_test_client(context)
+    app.authmodel('example/City', ['getall'])
+
+    resp = app.get('/example/City')
+    assert resp.status_code == 404
+
+
+def test_csv_tabular_seperator(rc: RawConfig, fs: AbstractFileSystem):
+    fs.pipe('cities.csv', (
+        'šalis;miestas\n'
+        'lt;Vilnius\n'
+        'lv;Ryga\n'
+        'ee;Talin'
+    ).encode('utf-8'))
+
+    context, manifest = prepare_manifest(rc, '''
+    d | r | b | m | property | type    | ref  | source              | prepare          | access
+    example                  |         |      |                     |                  |
+      | csv                  | csv     |      | memory://cities.csv | tabular(sep:";") |
+      |   |   | City         |         | name |                     |                  |
+      |   |   |   | name     | string  |      | miestas             |                  | open
+      |   |   |   | country  | string  |      | šalis               |                  | open
+    ''', mode=Mode.external)
+    context.loaded = True
+    app = create_test_client(context)
+    app.authmodel('example/City', ['getall'])
+
+    resp = app.get('/example/City')
+    assert listdata(resp, sort=False) == [
+        ('lt', 'Vilnius'),
+        ('lv', 'Ryga'),
+        ('ee', 'Talin'),
+    ]
 
 
 def test_xsd_base64(rc: RawConfig, tmp_path: Path):
