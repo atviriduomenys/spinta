0a129bb6585f88d0df0e277f6078907602e34d25
karina <karina.klinkeviciute@gmail.com>
2024-05-24 19:23:29 +0300

    edited notes for sql getone

--- notes/datasets/sql/getone.sh
+++ notes/datasets/sql/getone.sh
@@ -43,42 +43,10 @@ http GET "$SERVER/$DATASET/City?format(ascii)"
 #| d8444e1b-912d-470d-bf34-8306637854a5  ∅          WzJd   2   Kaunas
 
 http GET "$SERVER/$DATASET/City/47cb18cb-77e7-4216-852d-122c9214a3af"
-#| HTTP/1.1 500 Internal Server Error
-#| 
-#| {
-#|     "errors": [
-#|         {
-#|             "code": "NotImplementedError",
-#|             "message": "Could not find signature for getone: <Context, Model, Sql>"
-#|         }
-#|     ]
-#| }
-tail -100 $BASEDIR/spinta.log
-#| Traceback (most recent call last):
-#|   File "multipledispatch/dispatcher.py", line 269, in __call__
-#|     func = self._cache[types]
-#|            ~~~~~~~~~~~^^^^^^^
-#| KeyError: (
-#|     <class 'spinta.components.Context'>,
-#|     <class 'spinta.components.Model'>,
-#|     <class 'spinta.datasets.backends.sql.components.Sql'>,
-#| )
-#| 
-#| During handling of the above exception, another exception occurred:
-#| 
-#| Traceback (most recent call last):
-#|   File "gspinta/api.py", line 95, in homepage
-#|     return await create_http_response(context, params, request)
-#|            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#|   File "gspinta/utils/response.py", line 129, in create_http_response
-#|     return await commands.getone(
-#|            ^^^^^^^^^^^^^^^^^^^^^^
-#|   File "gspinta/commands/read.py", line 301, in getone
-#|     resp = await commands.getone(
-#|            ^^^^^^^^^^^^^^^^^^^^^^
-#|   File "gspinta/commands/read.py", line 354, in getone
-#|     data = commands.getone(context, model, backend, id_=params.pk)
-#|            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#|   File "multipledispatch/dispatcher.py", line 273, in __call__
-#|     raise NotImplementedError(
-#| NotImplementedError: Could not find signature for getone: <Context, Model, Sql>
+
+#{
+#    "_id": "47cb18cb-77e7-4216-852d-122c9214a3af",
+#    "_type": "datasets/sql/getone/City",
+#    "id": 1,
+#    "name": "Vilnius"
+#}
