efc5f78b2a89f97ca607d00ef4814dacd3d798a0
Tadas <tadas.stankevicius@atea.lt>
2024-10-09 08:50:56 +0300

    660 Add tests for sql

--- tests/dtypes/test_uuid.py
+++ tests/dtypes/test_uuid.py
@@ -1,12 +1,36 @@
-import pytest
 from pathlib import Path
+import sqlalchemy as sa
 import uuid
+
 from pytest import FixtureRequest
+import pytest
+
 from spinta.core.config import RawConfig
-from spinta.testing.client import create_test_client
+from spinta.testing.client import create_test_client, create_client
 from spinta.testing.data import listdata
 from spinta.testing.manifest import bootstrap_manifest
+from spinta.testing.datasets import create_sqlite_db
+from spinta.testing.datasets import Sqlite
+from spinta.testing.tabular import create_tabular_manifest
+from spinta.manifests.tabular.helpers import striptable
+from spinta.utils.types import is_str_uuid
 
+
+
+@pytest.fixture(scope='module')
+def uuid_db():
+    with create_sqlite_db({
+        'test_uuid': [
+            sa.Column('id', sa.String, primary_key=True, default=int),
+            sa.Column('guid', sa.String, default=lambda: str(uuid.uuid4())),
+        ],
+    }) as db:
+        db.write('test_uuid', [
+                    {'id': 1, 'guid': str(uuid.uuid4())},
+                    {'id': 2, 'guid': str(uuid.uuid4())},
+                    {'id': 3, 'guid': str(uuid.uuid4())},
+                ])
+        yield db
 
 @pytest.mark.manifests('internal_sql', 'csv')
 def test_uuid(
@@ -55,4 +79,22 @@ def test_uuid(
     })
     assert resp.status_code == 400
 
+def test_uuid_sql(context, rc: RawConfig, tmp_path: Path, uuid_db: Sqlite):
+    create_tabular_manifest(context, tmp_path / 'manifest.csv', striptable('''
+id | d | r | b | m | property  | type    | ref | source    | prepare | level | access | uri | title | description
+   | datasets/uuid/example     |         |     |           |         |       |        |     |       |
+   |   | data                  | sql     |     |           |         |       |        |     |       |
+   |                           |         |     |           |         |       |        |     |       |
+   |   |   |   | TestUUID      |         | id  | test_uuid |         |       | open   |     |       |
+   |   |   |   |   | id        | integer |     | id        |         |       |        |     |       |
+   |   |   |   |   | guid      | uuid    |     | guid      |         |       |        |     |       |
+    '''))
+
+    app = create_client(rc, tmp_path, uuid_db)
+
+    resp = app.get('/datasets/uuid/example/TestUUID')
+    assert resp.status_code == 200
+    data = resp.json()['_data']
+    assert len(data) == 3
+    assert all(is_str_uuid(item['guid']) for item in data)
 
