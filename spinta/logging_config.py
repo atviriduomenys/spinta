c05aa24a881fddf72d45ecb90b64c21b0943d7ea
ieva <i.braskute@gmail.com>
2024-10-24 12:12:09 +0300

    add logging config and some bug fixes

---
+++ spinta/logging_config.py
@@ -0,0 +1,8 @@
+import logging
+
+def setup_logging(level=logging.WARNING):
+    logging.basicConfig(
+        level=level,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[logging.StreamHandler()]
+    )
