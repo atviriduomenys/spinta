ec2bb8f6f48d793d21e26341ea16f5350c0418e8
Justinas Kenstavicius <justinas.kenstavicius@atea.lt>
2024-12-12 13:29:55 +0200

    P153 fixed ref init fkey constraint mapping

--- spinta/backends/postgresql/types/ref/init.py
+++ spinta/backends/postgresql/types/ref/init.py
@@ -1,17 +1,15 @@
 from typing import List, Union
 
 import sqlalchemy as sa
-
 from sqlalchemy.sql.type_api import TypeEngine
 
 from spinta import commands
-from spinta.backends.postgresql.helpers.name import get_pg_column_name
-from spinta.components import Context, Property
-from spinta.types.datatype import Ref
 from spinta.backends.helpers import get_table_name
 from spinta.backends.postgresql.components import PostgreSQL
-from spinta.backends.postgresql.helpers import get_pg_name
 from spinta.backends.postgresql.helpers import get_column_name
+from spinta.backends.postgresql.helpers.name import get_pg_column_name, get_pg_table_name
+from spinta.components import Context, Property
+from spinta.types.datatype import Ref
 
 
 @commands.prepare.register(Context, PostgreSQL, Ref)
@@ -22,7 +20,7 @@ def prepare(context: Context, backend: PostgreSQL, dtype: Ref, propagate: bool =
     if not dtype.inherited:
         columns = get_pg_foreign_key(
             dtype.prop,
-            table_name=get_pg_name(get_table_name(dtype.model)),
+            table_name=get_pg_table_name(get_table_name(dtype.model)),
             model_name=dtype.prop.model.name,
             column_type=pkey_type,
         )
@@ -61,7 +59,7 @@ def get_pg_foreign_key(
             unique=prop.dtype.unique
         ),
         sa.ForeignKeyConstraint(
-            [column_name], [get_pg_column_name(f'{table_name}._id')],
+            [column_name], [f'{table_name}._id'],
             ondelete='CASCADE' if prop.list else None,
             onupdate='CASCADE' if prop.list else None
         )
