cd1ab37cc394199adb65f1ef5474205322a09d3a
Justinas Kenstavicius <justinas.kenstavicius@atea.lt>
2025-02-19 15:16:37 +0200

    Releasing version 0.1.84

--- notes/spinta/release/patch.sh
+++ notes/spinta/release/patch.sh
@@ -15,9 +15,9 @@ git checkout $RELEASE_VERSION
 git pull
 git tag -l -n1 | sort -h | tail -n5
 
-export CURRENT_PATCH=82
-export NEW_PATCH=83
-export FUTURE_PATCH=84
+export CURRENT_PATCH=83
+export NEW_PATCH=84
+export FUTURE_PATCH=85
 
 export CURRENT_VERSION=$RELEASE_VERSION.$CURRENT_PATCH
 export NEW_VERSION=$RELEASE_VERSION.$NEW_PATCH
@@ -30,71 +30,13 @@ git status
 
 # notes/spinta/release/common.sh    Check outdated packages and upgrade them
 # Install
-#| Package operations: 0 installs, 39 updates, 0 removals
-#|
-#|   • Updating markupsafe (3.0.2 -> 2.1.5)
-#|   • Updating urllib3 (2.3.0 -> 2.2.3)
-#|   • Updating alabaster (0.7.16 -> 0.7.13)
-#|   • Updating docutils (0.21.2 -> 0.20.1)
-#|   • Updating sphinxcontrib-applehelp (2.0.0 -> 1.0.4)
-#|   • Updating sphinxcontrib-devhelp (2.0.0 -> 1.0.2)
-#|   • Updating sphinxcontrib-htmlhelp (2.1.0 -> 2.0.1)
-#|   • Updating sphinxcontrib-qthelp (2.0.0 -> 1.0.3)
-#|   • Updating sphinxcontrib-serializinghtml (2.0.0 -> 1.1.5)
-#|   • Updating numpy (2.0.2 -> 1.24.4)
-#|   • Updating prompt-toolkit (3.0.48 -> 3.0.50)
-#|   • Updating propcache (0.2.1 -> 0.2.0)
-#|   • Updating sphinx (7.4.7 -> 7.1.2)
-#|   • Updating zipp (3.21.0 -> 3.20.2)
-#|   • Updating aiosignal (1.3.2 -> 1.3.1)
-#|   • Updating anyio (4.8.0 -> 4.5.2)
-#|   • Updating cloudpickle (3.1.0 -> 3.1.1)
-#|   • Updating coverage (7.6.10 -> 7.6.1)
-#|   • Updating dnspython (2.7.0 -> 2.6.1)
-#|   • Updating ipython (8.18.1 -> 8.12.3)
-#|   • Updating isodate (0.7.2 -> 0.6.1)
-#|   • Updating pandas (2.2.3 -> 2.0.3)
-#|   • Updating partd (1.4.2 -> 1.4.1)
-#|   • Updating pyparsing (3.2.1 -> 3.1.4)
-#|   • Updating ruamel-yaml-clib (0.2.12 -> 0.2.8)
-#|   • Updating setuptools (75.7.0 -> 75.3.0)
-#|   • Updating yarl (1.18.3 -> 1.15.2)
-#|   • Updating aiohttp (3.11.11 -> 3.10.11)
-#|   • Updating dask (2024.8.0 -> 2023.5.0)
-#|   • Updating geoalchemy2 (0.16.0 -> 0.17.0)
-#|   • Updating objprint (0.3.0 -> 0.2.3)
-#|   • Updating phonenumbers (8.13.52 -> 8.13.53)
-#|   • Updating pyproj (3.6.1 -> 3.5.0)
-#|   • Updating pytest-cov (6.0.0 -> 5.0.0)
-#|   • Updating rdflib (7.1.1 -> 6.3.2)
-#|   • Updating responses (0.25.3 -> 0.25.6)
-#|   • Updating sphinx-autobuild (2024.10.3 -> 2021.3.14)
-#|   • Updating starlette (0.45.2 -> 0.44.0)
-#|   • Updating uvicorn (0.34.0 -> 0.33.0)
+#| No dependencies to install or update
 
 # Update
-#| Package operations: 0 installs, 15 updates, 0 removals
-#|
-#|   • Updating certifi (2024.12.14 -> 2025.1.31)
-#|   • Updating babel (2.16.0 -> 2.17.0)
-#|   • Updating executing (2.1.0 -> 2.2.0)
-#|   • Updating pytz (2024.2 -> 2025.1)
-#|   • Updating tzdata (2024.2 -> 2025.1)
-#|   • Updating attrs (24.3.0 -> 25.1.0)
-#|   • Updating fsspec (2024.12.0 -> 2025.2.0)
-#|   • Updating mako (1.3.8 -> 1.3.9)
-#|   • Updating psutil (6.1.1 -> 7.0.0)
-#|   • Updating cachetools (5.5.0 -> 5.5.1)
-#|   • Updating geoalchemy2 (0.17.0 -> 0.17.1)
-#|   • Updating lxml (5.3.0 -> 5.3.1)
-#|   • Updating phonenumbers (8.13.53 -> 8.13.55)
-#|   • Updating shapely (2.0.6 -> 2.0.7)
-#|   • Updating xlsxwriter (3.2.0 -> 3.2.2)
+#| No dependencies to install or update
 
 # Run Makefile
-cd docs
-make upgrade
-cd ..
+(cd docs && make upgrade)
 
 # Check what was changed and update CHANGES.rst
 xdg-open https://github.com/atviriduomenys/spinta/compare/$CURRENT_VERSION...$RELEASE_VERSION
@@ -106,7 +48,7 @@ head CHANGES.rst
 # notes/spinta/release/common.sh    Reset test database
 
 poetry run pytest -vvx --tb=short tests
-#| 2339 passed, 45 skipped, 58 warnings in 392.46s (0:06:32)
+#| 2339 passed, 45 skipped, 58 warnings in 406.50s (0:06:46)
 
 # If possible run same tests using test and prod env library versions
 # Test env
@@ -131,7 +73,7 @@ BASEDIR=$PWD/var/instances/$INSTANCE
 
 # notes/spinta/release/common.sh    Run server in EXTERNAL mode
 # notes/spinta/release/common.sh    Run migrations
-#| (3464 rows)
+#| (3476 rows)
 
 # notes/spinta/release/common.sh    Run server in INTERNAL mode
 # Don't forget to add client to server and credentials;
