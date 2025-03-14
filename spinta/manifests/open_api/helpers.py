04854360940588e50d9932e0a5b363664d2bb716
Gytis Petrauskas <petra.gytis@gmail.com>
2025-03-14 11:12:19 +0200

    [1208] OpenAPI Schemas: Dataset schema read

--- spinta/manifests/open_api/helpers.py
+++ spinta/manifests/open_api/helpers.py
@@ -7,19 +7,23 @@ from typing import Generator
 
 def read_file_data_and_transform_to_json(path: Path) -> dict:
     with Path(path).open() as file:
-        return json.load(file)
+        file_data = file.read()
+
+    return json.loads(file_data)
+
+
+def get_dataset_schema(info: dict) -> dict:
+    return {
+        'type': 'dataset',
+        'name': info['title'].lower().replace(' ', '_'),
+        'title': info.get('summary', info['title']),
+        'description': info.get('description')
+    }
 
 
 def read_open_api_manifest(path: Path) -> Generator[tuple[None, dict]]:
     data = read_file_data_and_transform_to_json(path)
 
+    # https://spec.openapis.org/oas/latest.html#info-object
     info = data['info']
-    title = info['title']
-    dataset_name = f'services/{title.lower().replace(" ", "_")}'
-
-    yield None, {
-        'type': 'dataset',
-        'name': dataset_name,
-        'title': info.get('summary', title),
-        'description': info.get('description', '')
-    }
+    yield None, get_dataset_schema(info)
