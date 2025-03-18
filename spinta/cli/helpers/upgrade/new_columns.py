c809970624887a22010391465889767432810b86
Karina Klinkevičiūtė <karina.klinkeviciute@gmail.com>
2025-03-19 00:11:33 +0200

    temporary files are copied to /tmp/

--- spinta/cli/helpers/upgrade/new_columns.py
+++ spinta/cli/helpers/upgrade/new_columns.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+import os
 import pathlib
+import shutil
+import tempfile
 from typing import Any
 
 from spinta.cli.manifest import copy_manifest
@@ -10,11 +13,17 @@ from spinta.components import Context
 def migrate_columns(context: Context, destructive: bool, **kwargs: Any):
     files_path_str = kwargs.get('files_path')
     files_path = pathlib.Path(files_path_str)
-    if files_path.is_file():
-        copy_manifest(context, output=files_path_str, manifests=[files_path_str])
-    else:
-        files = [str(file) for file in files_path.rglob('*') if file.is_file()]
-        for file in files:
-            copy_manifest(context, output=file, manifests=[file])
+    print(files_path_str)
+    with tempfile.TemporaryDirectory(dir="/tmp") as tmp_dir:
+        if files_path.is_file():
+            temp_file_str = os.path.join(tmp_dir, os.path.basename(files_path_str))
+            copy_manifest(context, output=temp_file_str, manifests=[files_path_str])
+            shutil.move(temp_file_str, files_path_str)
+        else:
+            files = [str(file) for file in files_path.rglob('*') if file.is_file()]
+            for file in files:
+                temp_file_str = os.path.join(tmp_dir, os.path.basename(file))
+                copy_manifest(context, output=temp_file_str, manifests=[file])
+                shutil.move(temp_file_str, file)
 
     print("DSA files upgraded succesfully")
--- tests/cli/test_upgrade.py
+++ tests/cli/test_upgrade.py
@@ -725,26 +725,25 @@ def test_upgrade_new_columns(
         'new_columns',
         # '-o', tmp_path / 'result.csv',
         '--files_path',
-        tmp_path / 'manifest.csv',
+        tmp_path,
+        # tmp_path / 'manifest.csv',
     ])
 
     manifest = load_manifest(rc, tmp_path / 'manifest.csv')
-    # assert manifest == '''
-    # d | r | b | m            | property | type    | ref         | source            | source.type  | prepare | level | access | uri | title | description | status | visibility | eli | count | origin
-    # datasets/gov/example     |          |         |             |                   |              |         |       |        |     |       |             |        |            |     |       |
-    #   | data                 | sql      |         | sqlite://   | sqlite            |              |         |       |        |     |       |             |        |            |     |       |
-    #                          |          |         |             |                   |              |         |       |        |     |       |             |        |            |     |       |
-    #   |   |   | Country      |          | code    | salis       | table             |              |         |       |        |     |       |             |        |            |     |       |
-    #   |   |   |   | code     | integer  |         | kodas       | test              |              |         |       |        |     |       |             |        |            |     |       |
-    #   |   |   |   | name     | string   |         | pavadinimas | varchar(255)      |              |         |       |        |     |       |             |        |            |     |       |
-    #   |   |   |   | driving  | string   |         | vairavimas  | varchar(1)        |              |         |       |        |     |       |             |        |            |     |       |
-    #                          | enum     |         | l           |                   |              |         |       |        |     |       |             |        |            |     |       |
-    #                          |          |         | r           |                   |              |         |       |        |     |       |             |        |            |     |       |
-    #                          |          |         |             |                   |              |         |       |        |     |       |             |        |            |     |       |
-    #   |   |   | City         |          | name    | miestas     | view materialized |              |         |       |        |     |       |             |        |            |     |       |
-    #   |   |   |   | name     | string   |         | pavadinimas | varchar(255)      |              |         |       |        |     |       |             |        |            |     |       |
-    #   |   |   |   | country  | ref      | Country | salis       | integer           |              |         |       |        |     |       |             |        |            |     |       |
-    # '''
 
     assert manifest == """
+id | d | r | b | m | property | type    | ref     | source      | source.type       | prepare | level | access | uri | title | description | status | visibility | eli | count | origin
+   | datasets/gov/example     |         |         |             |                   |         |       |        |     |       |             |        |            |     |       |
+   |   | data                 | sql     |         | sqlite://   | sqlite            |         |       |        |     |       |             |        |            |     |       |
+   |                          |         |         |             |                   |         |       |        |     |       |             |        |            |     |       |
+   |   |   |   | Country      |         | code    | salis       | table             |         |       |        |     |       |             |        |            |     |       |
+   |   |   |   |   | code     | integer |         | kodas       | test              |         |       |        |     |       |             |        |            |     |       |
+   |   |   |   |   | name     | string  |         | pavadinimas | varchar(255)      |         |       |        |     |       |             |        |            |     |       |
+   |   |   |   |   | driving  | string  |         | vairavimas  | varchar(1)        |         |       |        |     |       |             |        |            |     |       |
+   |                          | enum    |         | l           |                   |         |       |        |     |       |             |        |            |     |       |
+   |                          |         |         | r           |                   |         |       |        |     |       |             |        |            |     |       |
+   |                          |         |         |             |                   |         |       |        |     |       |             |        |            |     |       |
+   |   |   |   | City         |         | name    | miestas     | view materialized |         |       |        |     |       |             |        |            |     |       |
+   |   |   |   |   | name     | string  |         | pavadinimas | varchar(255)      |         |       |        |     |       |             |        |            |     |       |
+   |   |   |   |   | country  | ref     | Country | salis       | integer           |         |       |        |     |       |             |        |            |     |       |
     """
