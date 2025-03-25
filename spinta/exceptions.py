d919f39172e4a36f6f7f295b2f896578f1266d37
varzgalys <varzgalys.tomas@gmail.com>
2025-03-25 19:56:33 +0200

    1196: add model name to UnableToMapIntermediateTable error message, tests.

--- spinta/exceptions.py
+++ spinta/exceptions.py
@@ -995,7 +995,7 @@ class MissingRefModel(UserError):
 
 
 class UnableToMapIntermediateTable(UserError):
-    template = 'Unable to map intermediate table to `Array` property.'
+    template = 'Unable to map intermediate table ({model}) to `Array` property.'
 
 
 class InvalidIntermediateTableMappingRefCount(UserError):
