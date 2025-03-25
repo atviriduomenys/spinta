6e5a89a9c4705993b0387262522c3d4e7919eb84
mindaugasstanionis <mindaugas.stanionis@cornercasetech.com>
2025-03-25 12:09:31 +0200

    1229: adding getone functionality, splitting duplicate code into seperate function

--- spinta/utils/get.py
+++ spinta/utils/get.py
@@ -1,27 +1,56 @@
-import json
+from typing import Any
+
 import yaml
 
 from spinta import commands
 from spinta.cli.helpers.manifest import convert_str_to_manifest_path
+from spinta.components import Context
 from spinta.core.context import configure_context, create_context
 
 
-def get_json_content_from_yaml(yaml_content: str, manifests: list[str]) -> str:
-    records = yaml.safe_load(yaml_content)
+def initialize_context_and_store(
+    manifests: list[str], records: Any
+) -> tuple[Context, Any]:
     ctx = create_context()
+
     manifests = convert_str_to_manifest_path(manifests)
     context = configure_context(ctx, manifests)
+
     config = context.get("config")
     commands.load(context, config)
     commands.check(context, config)
+
     store = context.get("store")
     commands.load(context, store)
     commands.load(context, store.manifest)
+
     context.set("yaml_content", records)
+
+    return context, store
+
+
+def getall_json_content_from_yaml(
+    manifests: list[str], yaml_content: str, model_name: str
+) -> dict:
+    records = yaml.safe_load(yaml_content)
+    context, store = initialize_context_and_store(manifests, records)
     commands.prepare(context, store.manifest)
     response_dict = {}
-    for model in commands.get_models(context, store.manifest).values():
-        value = list(commands.getall(context, model, store.manifest.backend))
-        if value:
-            response_dict[value[0]["_type"]] = value
-    return json.dumps(response_dict)
+    model = commands.get_model(context, store.manifest, model_name)
+    response_dict["_data"] = list(
+        commands.getall(context, model, store.manifest.backend)
+    )
+    return response_dict
+
+
+def getone_json_content_from_yaml(
+    manifests: list[str], yaml_content: str, model_name: str, id_: str
+) -> dict:
+    records = yaml.safe_load(yaml_content)
+    context, store = initialize_context_and_store(manifests, records)
+    commands.prepare(context, store.manifest)
+    model = commands.get_model(context, store.manifest, model_name)
+    try:
+        return commands.getone(context, model, store.manifest.backend, id_=id_)
+    except KeyError:
+        return {}
--- tests/utils/test_get.py
+++ tests/utils/test_get.py
@@ -6,7 +6,10 @@ from yaml.scanner import ScannerError
 from spinta.components import Context
 from spinta.manifests.tabular.helpers import striptable
 from spinta.testing.tabular import create_tabular_manifest
-from spinta.utils.get import get_json_content_from_yaml
+from spinta.utils.get import (
+    getall_json_content_from_yaml,
+    getone_json_content_from_yaml,
+)
 
 
 @pytest.fixture
@@ -47,22 +50,39 @@ def yaml_content():
     """
 
 
-def test_get_json_content_from_yaml_success(context: Context, manifest_file: Path):
-    json_content = get_json_content_from_yaml(yaml_content(), [str(manifest_file)])
-    assert (
-        json_content
-        == """{"datasets/gov/example/Country": [{"_type": "datasets/gov/example/Country", "_id": "dac60010-9a29-4201-8da9-e624023eb626", "name": "Lietuva"}], "datasets/gov/example/City": [{"_type": "datasets/gov/example/City", "_id": "ca117a8a-fdd0-4c66-9565-093d4d493e6b", "name": "Vilnius", "country": {"_id": "dac60010-9a29-4201-8da9-e624023eb626", "name": "Lietuva"}}]}"""
+def test_getall_json_content_from_yaml_success(context: Context, manifest_file: Path):
+    json_content = getall_json_content_from_yaml(
+        [str(manifest_file)], yaml_content(), "datasets/gov/example/City"
     )
+    assert json_content == {
+        "_data": [
+            {
+                "_type": "datasets/gov/example/City",
+                "_id": "ca117a8a-fdd0-4c66-9565-093d4d493e6b",
+                "name": "Vilnius",
+                "country": {
+                    "_id": "dac60010-9a29-4201-8da9-e624023eb626",
+                    "name": "Lietuva",
+                },
+            }
+        ]
+    }
 
 
-def test_get_json_content_from_yaml_invalid_yaml(context: Context, manifest_file: Path):
+def test_getall_json_content_from_yaml_invalid_yaml(
+    context: Context, manifest_file: Path
+):
     yaml_with_error = yaml_content().replace("_type:", "_type")
 
     with pytest.raises(ScannerError):
-        get_json_content_from_yaml(yaml_with_error, [str(manifest_file)])
+        getall_json_content_from_yaml(
+            [str(manifest_file)], yaml_with_error, "datasets/gov/example/City"
+        )
 
 
-def test_get_json_content_from_yaml_yaml_without_id(context: Context, manifest_file: Path):
+def test_getall_json_content_from_yaml_yaml_without_id(
+    context: Context, manifest_file: Path
+):
     yaml_content = """
         - _type: datasets/gov/example/City
           name: Vilnius
@@ -71,8 +91,48 @@ def test_get_json_content_from_yaml_yaml_without_id(context: Context, manifest_f
     
         - _type: datasets/gov/example/Country
           name: Lietuva"""
-    json_content = json.loads(get_json_content_from_yaml(yaml_content, [str(manifest_file)]))
+    json_content = getall_json_content_from_yaml(
+        [str(manifest_file)], yaml_content, "datasets/gov/example/City"
+    )
 
-    for key, items in json_content.items():
+    for _, items in json_content.items():
         for item in items:
             assert "_id" in item
+
+
+def test_getone_json_content_from_yaml_success(context: Context, manifest_file: Path):
+    json_content = getone_json_content_from_yaml(
+        [str(manifest_file)],
+        yaml_content(),
+        "datasets/gov/example/City",
+        "ca117a8a-fdd0-4c66-9565-093d4d493e6b",
+    )
+    assert json_content == {
+        "_type": "datasets/gov/example/City",
+        "_id": "ca117a8a-fdd0-4c66-9565-093d4d493e6b",
+        "name": "Vilnius",
+        "country": {"_id": "dac60010-9a29-4201-8da9-e624023eb626", "name": "Lietuva"},
+    }
+
+
+def test_getone_json_content_from_yaml_invalid_yaml(
+    context: Context, manifest_file: Path
+):
+    yaml_with_error = yaml_content().replace("_type:", "_type")
+
+    with pytest.raises(ScannerError):
+        getone_json_content_from_yaml(
+            [str(manifest_file)],
+            yaml_with_error,
+            "datasets/gov/example/City",
+            "ca117a8a-fdd0-4c66-9565-093d4d493e6b",
+        )
+
+
+def test_getone_json_content_from_yaml_no_match_returns_empty_dict(
+    context: Context, manifest_file: Path
+):
+    json_content = getone_json_content_from_yaml(
+        [str(manifest_file)], yaml_content(), "datasets/gov/example/City", "wrong_id"
+    )
+    assert json_content == {}
