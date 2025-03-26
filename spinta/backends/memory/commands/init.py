df2ab4be3a3a8e28648a9f3333dde51771a24237
mindaugasstanionis <mindaugas.stanionis@cornercasetech.com>
2025-03-26 10:26:31 +0200

    1229: adding check in memory backend prepare command

--- spinta/backends/memory/commands/init.py
+++ spinta/backends/memory/commands/init.py
@@ -6,13 +6,16 @@ from spinta.backends.helpers import get_table_name
 from spinta.backends.memory.components import Memory
 import yaml
 
+
 @commands.prepare.register(Context, Memory, Manifest)
 def prepare(context: Context, backend: Memory, manifest: Manifest, **kwargs):
     for model in commands.get_models(context, manifest).values():
         backend.create(get_table_name(model))
         backend.create(get_table_name(model, TableType.CHANGELOG))
-    with open(backend.dsn, 'r') as file:
-        data = yaml.safe_load(file)
+    if hasattr(backend, "dsn"):
+        with open(backend.dsn, "r") as file:
+            data = yaml.safe_load(file)
 
-    for record in data:
-        backend.insert(record)
+        if data is not None:
+            for record in data:
+                backend.insert(record)
