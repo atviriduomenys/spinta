524894033aa325508075a49f00e2eb371aea3763
mindaugasstanionis <mindaugas.stanionis@cornercasetech.com>
2025-04-10 09:18:34 +0300

    1229: adding default args to Argument obj

--- spinta/cli/get.py
+++ spinta/cli/get.py
@@ -15,9 +15,9 @@ log = logging.getLogger(__name__)
 
 def getall(
     ctx: TyperContext,
-    manifests: Optional[List[str]] = Argument(help=("Manifest files to load")),
-    backend: str = Argument(help=("Backend connection string")),
-    model: str = Argument(help=("Model path")),
+    manifests: Optional[List[str]] = Argument(None, help=("Manifest files to load")),
+    backend: str = Argument(None, help=("Backend connection string")),
+    model: str = Argument(None, help=("Model path")),
 ):
     manifests = convert_str_to_manifest_path(manifests)
     context = configure_context(
@@ -36,9 +36,9 @@ def getall(
 def getone(
     ctx: TyperContext,
     manifests: Optional[List[str]] = Argument(help=("Manifest files to load")),
-    backend: str = Argument(help=("Backend connection string")),
-    model: str = Argument(help=("Model path")),
-    id_: str = Argument(help=("Dataset model id")),
+    backend: str = Argument(None, help=("Backend connection string")),
+    model: str = Argument(None, help=("Model path")),
+    id_: str = Argument(None, help=("Dataset model id")),
 ):
     manifests = convert_str_to_manifest_path(manifests)
     context = configure_context(
