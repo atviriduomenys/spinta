3af414161a16c04a668fe461b8c3bea97ff1f44f
Karina Klinkevičiūtė <karina.klinkeviciute@gmail.com>
2025-06-02 19:15:04 +0300

    #1326 fixed a callable bug for python>3.10

--- spinta/cli/helpers/upgrade/components.py
+++ spinta/cli/helpers/upgrade/components.py
@@ -1,15 +1,14 @@
-from __future__ import annotations
-
 import enum
 from collections.abc import Callable
 
-from typing_extensions import ParamSpec
+from typing import Concatenate, ParamSpec
 
 from spinta.components import Context
 
-P = ParamSpec("P")
-UpgradeFuncType = Callable[P.kwargs]
-UpgradeCheckFuncType = Callable[P.kwargs, bool]
+P = ParamSpec('P')
+
+UpgradeFuncType = Callable[Concatenate[Context, P], None]
+UpgradeCheckFuncType = Callable[Concatenate[Context, P], bool]
 
 
 class UpgradeComponent:
