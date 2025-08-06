e0f557be802bf14ab508898f0da468928a7a1c77
tutis <tautvydas@cornercasetech.com>
2025-08-06 13:13:24 +0300

    failu surinkimas ir sudejimas i serveri

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
+
 CONFIG_FILE="config.yml"
+
+if [ ! -f "$CONFIG_FILE" ]; then
+    export SPINTA_CONFIG=config.yml
+    cat > "$CONFIG_FILE" <<EOF
+env: test
+data_path: $PWD/$BASEDIR
+default_auth_client: default
+backends:
+  default:
+    type: postgresql
+    dsn: postgresql://admin:admin123@${DB_HOST:=localhost}:${DB_PORT:=54321}/spinta
+accesslog:
+  type: file
+  file: stdout
+EOF
+    echo "Created config.yml."
+else
+    echo "Found existing config.yml."
+fi
 
 git clone https://github.com/atviriduomenys/demo-saltiniai.git
 mkdir manifests
@@ -9,37 +29,16 @@ ls manifests/* | xargs spinta copy -o manifest.csv
 
 rm -rf demo-saltiniai manifests
 
-if [ ! -f "$CONFIG_FILE" ]; then
-    export SPINTA_CONFIG=config.yml
-    cat > "$CONFIG_FILE" <<EOF
-env: test
-data_path: $PWD/$BASEDIR
-default_auth_client: default
-keymaps:
-  default:
-    type: sqlalchemy
-    dsn: sqlite:///$PWD/$BASEDIR/keymap.db
-backends:
-  default:
-    type: postgresql
-    dsn: postgresql://admin:admin123@${DB_HOST:=localhost}:${DB_PORT:=54321}/spinta
-accesslog:
-  type: file
-  file: stdout
+cat > "$CONFIG_FILE" <<EOF
 manifest: default
 manifests:
   default:
     type: csv
-    path: $PWD/$BASEDIR/manifest.csv
+    path: /app/manifest.csv
     backend: default
     keymap: default
     mode: external
 EOF
-    echo "Created config.yml."
-else
-    echo "Found existing config.yml."
-fi
 
-poetry install --all
 spinta upgrade
 make run
