88197d762bf1476798d97c3d6624985cdbe29997
Gytis Petrauskas <petra.gytis@gmail.com>
2025-08-14 14:19:33 +0300

    [1415] Spinta sync improvements: Refactor current implementation
    - Checkpoint 1: Move logic to methods

--- spinta/cli/sync.py
+++ spinta/cli/sync.py
@@ -42,17 +42,17 @@ def _get_file_bytes_and_decoded_content(manifests: List[ManifestPath]) -> Tuple[
     return file_bytes, content
 
 
-def _get_base_path_and_authorization_headers(context: Context) -> Tuple[str, Dict[str, str]]:
+def _get_base_path_and_headers(context: Context) -> Tuple[str, Dict[str, str]]:
     config: Config = context.get("config")
     credentials: RemoteClientCredentials = get_client_credentials(config.credentials_file, DEFAULT_CREDENTIALS_SECTION)
 
     access_token = get_access_token(credentials)
-    authorization_headers = {"Authorization": f"Bearer {access_token}"}
+    headers = {"Authorization": f"Bearer {access_token}"}
 
     resource_server = credentials.resource_server or credentials.server
     base_path = f"{resource_server}/uapi/datasets/org/vssa/isris/dcat"
 
-    return base_path, authorization_headers
+    return base_path, headers
 
 
 def _format_error_response_data(data: dict) -> dict:
@@ -60,102 +60,141 @@ def _format_error_response_data(data: dict) -> dict:
     return data
 
 
+def get_manifest_paths(manifests: List[str]):
+    manifests = convert_str_to_manifest_path(manifests)
+    if not manifests:
+        raise ManifestFileNotProvided
+
+    return manifests
+
+
+def build_manifest_and_context(ctx: TyperContext, manifests: List[str]) -> Tuple[Context, Manifest]:
+    context = configure_context(ctx.obj, manifests, mode=Mode.external)
+    store = prepare_manifest(context, verbose=False, full_load=True)
+    return context, store.manifest
+
+
+def get_dataset(base_path, headers, dataset_name):
+    response = requests.get(
+        f"{base_path}/Dataset/",
+        headers=headers,
+        params={"name": dataset_name},
+    )
+
+    if response.status_code not in {HTTPStatus.OK, HTTPStatus.NOT_FOUND}:
+        raise UnexpectedAPIResponse(
+            operation="Get dataset",
+            expected_status_code={HTTPStatus.OK, HTTPStatus.NOT_FOUND},
+            response_status_code=response.status_code,
+            response_data=_format_error_response_data(response.json()),
+        )
+
+    return response
+
+
+def extract_dataset_id(response, response_type):
+    dataset_id = None
+    if response_type == "list":
+        dataset_id = response.json().get("_data", [{}])[0].get(IDENTIFIER)
+    elif response_type == "detail":
+        dataset_id = response.json().get(IDENTIFIER)
+
+    if not dataset_id:
+        raise UnexpectedAPIResponseData(
+            operation=f"Retrieve dataset `{IDENTIFIER}`",
+            context=f"Dataset did not return the `{IDENTIFIER}` field which can be used to identify the dataset."
+        )
+
+    return dataset_id
+
+
+def update_dsa(base_path, headers, dataset_id):
+    # TODO: Unfinished: implement w/ https://github.com/atviriduomenys/katalogas/issues/1600.
+    response = requests.put(f"{base_path}/Dataset/{dataset_id}/dsa/", headers=headers)
+    raise NotImplementedFeature(
+        status=response.status_code,
+        dataset_id=dataset_id,
+        feature="Updates on existing Datasets",
+    )
+
+
+def create_dataset(base_path, headers, dataset_name):
+    response = requests.post(
+        f"{base_path}/Dataset/",
+        headers=headers,
+        data={
+            "title": dataset_name,
+            "description": "",
+            "name": dataset_name,
+        }
+    )
+
+    if response.status_code != HTTPStatus.CREATED:
+        raise UnexpectedAPIResponse(
+            operation="Create dataset",
+            expected_status_code=HTTPStatus.CREATED,
+            response_status_code=response.status_code,
+            response_data=_format_error_response_data(response.json()),
+        )
+
+    return response
+
+
+def create_distribution(base_path, headers, dataset_name, file_bytes, dataset_id, manifests):
+    response = requests.post(
+        f"{base_path}/Distribution/",
+        headers=headers,
+        data={
+            "dataset": dataset_id,
+            "title": dataset_name,
+        },
+        files={
+            "file": (manifests[0].path, BytesIO(file_bytes), CONTENT_TYPE_TEXT_CSV),
+        }
+    )
+
+    if response.status_code != HTTPStatus.CREATED:
+        raise UnexpectedAPIResponse(
+            operation="Create dataset distribution",
+            expected_status_code=HTTPStatus.CREATED,
+            response_status_code=response.status_code,
+            response_data=_format_error_response_data(response.json()),
+        )
+
+
+def create_dsa(base_path, headers, dataset_id, content):
+    response = requests.post(
+        f"{base_path}/Dataset/{dataset_id}/dsa/",
+        headers={"Content-Type": CONTENT_TYPE_TEXT_CSV, **headers},
+        data=content,
+    )
+
+    if response.status_code != HTTPStatus.NO_CONTENT:
+        raise UnexpectedAPIResponse(
+            operation="Create DSA for dataset",
+            expected_status_code=HTTPStatus.CREATED,
+            response_status_code=response.status_code,
+            response_data=_format_error_response_data(response.json()),
+        )
+
+
 def sync(
     ctx: TyperContext,
     manifests: List[str] = Argument(None, help=("Manifest files to load")),
 ):
-    manifests = convert_str_to_manifest_path(manifests)
-    if not manifests:
-        raise ManifestFileNotProvided
-    context = configure_context(ctx.obj, manifests, mode=Mode.external)
-    store = prepare_manifest(context, verbose=False, full_load=True)
-    manifest: Manifest = store.manifest
+    manifests = get_manifest_paths(manifests)
+    context, manifest = build_manifest_and_context(ctx, manifests)
 
     dataset_name = _get_dataset_name(context, manifest)
     file_bytes, content = _get_file_bytes_and_decoded_content(manifests)
-    base_path, authorization_headers = _get_base_path_and_authorization_headers(context)
+    base_path, headers = _get_base_path_and_headers(context)
 
-    response_get_dataset = requests.get(
-        f"{base_path}/Dataset/",
-        headers=authorization_headers,
-        params={"name": dataset_name},
-    )
-    if response_get_dataset.status_code not in {HTTPStatus.OK, HTTPStatus.NOT_FOUND}:
-        raise UnexpectedAPIResponse(
-            operation="Get dataset",
-            expected_status_code={HTTPStatus.OK, HTTPStatus.NOT_FOUND},
-            response_status_code=response_get_dataset.status_code,
-            response_data=_format_error_response_data(response_get_dataset.json()),
-        )
-
+    response_get_dataset = get_dataset(base_path, headers, dataset_name)
     if response_get_dataset.status_code == HTTPStatus.OK:
-        if not (dataset_id := response_get_dataset.json().get("_data", [{}])[0].get(IDENTIFIER)):
-            raise UnexpectedAPIResponseData(
-                operation=f"Retrieve dataset `{IDENTIFIER}`",
-                context=f"Dataset did not return the `{IDENTIFIER}` field which can be used to identify the dataset."
-            )
-        response_update_dsa = requests.put(f"{base_path}/Dataset/{dataset_id}/dsa/", headers=authorization_headers)
-        # TODO: Implement w/ https://github.com/atviriduomenys/katalogas/issues/1600.
-        raise NotImplementedFeature(
-            status=response_update_dsa.status_code,
-            dataset_id=dataset_id,
-            feature="Updates on existing Datasets",
-        )
+        dataset_id = extract_dataset_id(response_get_dataset, "list")
+        update_dsa(base_path, headers, dataset_id)
     elif response_get_dataset.status_code == HTTPStatus.NOT_FOUND:
-        response_post_dataset = requests.post(
-            f"{base_path}/Dataset/",
-            headers=authorization_headers,
-            data={
-                "title": dataset_name,
-                "description": "",
-                "name": dataset_name,
-            }
-        )
-
-        if response_post_dataset.status_code != HTTPStatus.CREATED:
-            raise UnexpectedAPIResponse(
-                operation="Create dataset",
-                expected_status_code=HTTPStatus.CREATED,
-                response_status_code=response_post_dataset.status_code,
-                response_data=_format_error_response_data(response_post_dataset.json()),
-            )
-
-        dataset_id = response_post_dataset.json().get(IDENTIFIER)
-        if not dataset_id:
-            raise UnexpectedAPIResponseData(
-                operation=f"Retrieve dataset `{IDENTIFIER}`",
-                context=f"Dataset did not return the `{IDENTIFIER}` field which can be used to identify the dataset."
-            )
-        response_post_distribution = requests.post(
-            f"{base_path}/Distribution/",
-            headers=authorization_headers,
-            data={
-                "dataset": dataset_id,
-                "title": dataset_name,
-            },
-            files={
-                "file": (manifests[0].path, BytesIO(file_bytes), CONTENT_TYPE_TEXT_CSV),
-            }
-        )
-
-        if response_post_distribution.status_code != HTTPStatus.CREATED:
-            raise UnexpectedAPIResponse(
-                operation="Create dataset distribution",
-                expected_status_code=HTTPStatus.CREATED,
-                response_status_code=response_post_distribution.status_code,
-                response_data=_format_error_response_data(response_post_distribution.json()),
-            )
-
-        response_create_dsa = requests.post(
-            f"{base_path}/Dataset/{dataset_id}/dsa/",
-            headers={"Content-Type": CONTENT_TYPE_TEXT_CSV, **authorization_headers},
-            data=content,
-        )
-
-        if response_create_dsa.status_code != HTTPStatus.NO_CONTENT:
-            raise UnexpectedAPIResponse(
-                operation="Create DSA for dataset",
-                expected_status_code=HTTPStatus.CREATED,
-                response_status_code=response_create_dsa.status_code,
-                response_data=_format_error_response_data(response_create_dsa.json()),
-            )
+        response_create_dataset = create_dataset(base_path, headers, dataset_name)
+        dataset_id = extract_dataset_id(response_create_dataset, "detail")
+        create_distribution(base_path, headers, dataset_name, file_bytes, dataset_id, manifests)
+        create_dsa(base_path, headers, dataset_id, content)
