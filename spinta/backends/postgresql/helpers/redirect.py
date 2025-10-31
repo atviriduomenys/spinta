1e77c38e8dda77b09408fbdb4ae98ccdb448199d
Justinas Kenstavicius <justinas.kenstavicius@atea.lt>
2025-10-31 09:19:01 +0200

    1579 added postgresql_comment upgrade script

--- spinta/backends/postgresql/helpers/redirect.py
+++ spinta/backends/postgresql/helpers/redirect.py
@@ -8,6 +8,7 @@ from spinta import commands
 from spinta.backends.constants import TableType
 from spinta.backends.helpers import get_table_name
 from spinta.backends.postgresql.helpers import get_pg_name
+from spinta.backends.postgresql.helpers.name import get_pg_column_name
 from spinta.components import Context, Model
 
 if TYPE_CHECKING:
@@ -15,13 +16,14 @@ if TYPE_CHECKING:
 
 
 def get_redirect_table(context: Context, backend: PostgreSQL, model: Model):
-    table_name = get_pg_name(get_table_name(model, TableType.REDIRECT))
+    table_name = get_table_name(model, TableType.REDIRECT)
     pkey_type = commands.get_primary_key_type(context, backend)
     table = sa.Table(
-        table_name,
+        get_pg_name(table_name),
         backend.schema,
-        sa.Column("_id", pkey_type, primary_key=True),
-        sa.Column("redirect", pkey_type, index=True),
+        sa.Column(get_pg_column_name("_id"), pkey_type, primary_key=True, comment="_id"),
+        sa.Column(get_pg_column_name("redirect"), pkey_type, index=True, comment="redirect"),
+        comment=table_name,
     )
     return table
 
--- spinta/backends/postgresql/types/array/init.py
+++ spinta/backends/postgresql/types/array/init.py
@@ -3,6 +3,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects.postgresql import JSONB
 
 from spinta import commands
+from spinta.backends.postgresql.helpers.name import get_pg_column_name
 from spinta.components import Context
 from spinta.types.datatype import Array
 from spinta.backends.constants import TableType
@@ -33,27 +34,29 @@ def prepare(context: Context, backend: PostgreSQL, dtype: Array, **kwargs):
     #             f'{parent_list_table_name}._id', ondelete='CASCADE',
     #         )),
     #     ]
-    name = get_pg_name(get_table_name(prop, TableType.LIST))
+    name = get_table_name(prop, TableType.LIST)
     main_table_name = get_pg_name(get_table_name(prop.model))
     table = sa.Table(
-        name,
+        get_pg_name(name),
         backend.schema,
         # TODO: List tables eventually will have _id in order to uniquelly
         #       identify list item.
         # sa.Column('_id', pkey_type, primary_key=True),
-        sa.Column("_txn", pkey_type, index=True),
+        sa.Column(get_pg_column_name("_txn"), pkey_type, index=True, comment="_txn"),
         sa.Column(
-            "_rid",
+            get_pg_column_name("_rid"),
             pkey_type,
             sa.ForeignKey(
                 f"{main_table_name}._id",
                 ondelete="CASCADE",
             ),
+            comment="_rid",
         ),
         *columns,
+        comment=name,
     )
     backend.add_table(table, prop, TableType.LIST)
 
     if prop.list is None:
         # For fast whole resource access we also store whole list in a JSONB.
-        return sa.Column(prop.place, JSONB)
+        return sa.Column(get_pg_column_name(prop.place), JSONB, comment=prop.place)
