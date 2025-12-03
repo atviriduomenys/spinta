474a7bf76ecc77dd3efeed514de353fa28890112
Karina Klinkevičiūtė <karina.klinkeviciute@gmail.com>
2025-12-03 18:48:34 +0200

    prepare new version

--- notes/spinta/release/pre_release.sh
+++ notes/spinta/release/pre_release.sh
@@ -7,9 +7,9 @@ test -n "$PID" && kill "$PID"
 
 # Setup versions and create prepare branch
 export MAJOR=0
-export MINOR=2dev10
-export OLD_MINOR=2dev9
-export FUTURE_MINOR=2dev11
+export MINOR=2dev11
+export OLD_MINOR=2dev10
+export FUTURE_MINOR=2dev12
 export RELEASE_VERSION=$MAJOR.$MINOR
 export CURRENT_VERSION=$MAJOR.$OLD_MINOR
 export FUTURE_VERSION=$MAJOR.$FUTURE_MINOR
@@ -20,7 +20,7 @@ git checkout master
 git pull
 git tag -l -n1 | sort -h | tail -n5
 
-export PREPARE_BRANCH=prepare_${NEW_VERSION}_version
+export PREPARE_BRANCH=release_${NEW_VERSION}_version
 git branch $PREPARE_BRANCH
 git checkout $PREPARE_BRANCH
 git status
