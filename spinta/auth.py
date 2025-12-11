e46eb9b0b918221311e3b65b18bb04fe66a3317c
Martynas Raila <martynas.raila@cornercasetech.com>
2025-12-11 14:06:20 +0200

    1598 token.check_scope(...) return True if scope is valid

--- spinta/auth.py
+++ spinta/auth.py
@@ -402,33 +402,36 @@ class Token(rfc6749.TokenMixin):
         required_scopes = scope_to_list(scope)
         return not self._validator.scope_insufficient(self.get_scope(), required_scopes)
 
-    def check_scope(self, scope: SCOPE_TYPE):
+    def check_scope(self, scope: SCOPE_TYPE) -> bool:
         token_scopes = set(scope_to_list(self._token.get("scope", "")))
         if any(token_scope for token_scope in token_scopes if token_scope.startswith(DEPRECATED_SCOPE_PREFIX)):
             log.warning(
                 "Deprecation warning: using 'spinta_*' scopes is deprecated and will be removed in a future version."
             )
 
-        if not self.valid_scope(scope):
-            client_id = self._token["aud"]
+        if self.valid_scope(scope):
+            return True
 
-            operator = "OR"
-            if isinstance(scope, str):
-                operator = "AND"
-                scope = [scope]
-            missing_scopes = ", ".join(
-                sorted([single_scope for single_scope in scope if not single_scope.startswith(DEPRECATED_SCOPE_PREFIX)])
-            )
+        # Scope is not valid. Raise an exception
+        client_id = self._token["aud"]
 
-            # FIXME: this should be wrapped into UserError.
-            if operator == "AND":
-                log.error(f"client {client_id!r} is missing required scopes: %s", missing_scopes)
-                raise InsufficientScopeError(description=f"Missing scopes: {missing_scopes}")
-            elif operator == "OR":
-                log.error(f"client {client_id!r} is missing one of required scopes: %s", missing_scopes)
-                raise InsufficientScopeError(description=f"Missing one of scopes: {missing_scopes}")
-            else:
-                raise Exception(f"Unknown operator {operator}.")
+        operator = "OR"
+        if isinstance(scope, str):
+            operator = "AND"
+            scope = [scope]
+        missing_scopes = ", ".join(
+            sorted([single_scope for single_scope in scope if not single_scope.startswith(DEPRECATED_SCOPE_PREFIX)])
+        )
+
+        # FIXME: this should be wrapped into UserError.
+        if operator == "AND":
+            log.error(f"client {client_id!r} is missing required scopes: %s", missing_scopes)
+            raise InsufficientScopeError(description=f"Missing scopes: {missing_scopes}")
+        elif operator == "OR":
+            log.error(f"client {client_id!r} is missing one of required scopes: %s", missing_scopes)
+            raise InsufficientScopeError(description=f"Missing one of scopes: {missing_scopes}")
+        else:
+            raise Exception(f"Unknown operator {operator}.")
 
     # No longer mandatory, but will keep it, since it is used in other places.
     def get_client_id(self) -> str:
@@ -466,7 +469,7 @@ class AdminToken(rfc6749.TokenMixin):
     def valid_scope(self, scope: SCOPE_TYPE, **kwargs) -> bool:
         return True
 
-    def check_scope(self, scope: SCOPE_TYPE, **kwargs):
+    def check_scope(self, scope: SCOPE_TYPE, **kwargs) -> bool:
         pass
 
     def get_sub(self) -> str:  # User.
@@ -690,14 +693,14 @@ def get_client_file_path(path: pathlib.Path, client: str) -> pathlib.Path:
     return client_file
 
 
-def check_scope(context: Context, scope: Union[Scopes, str]):
+def check_scope(context: Context, scope: Union[Scopes, str]) -> bool:
     config = context.get("config")
     token = context.get("auth.token")
 
     if isinstance(scope, Scopes):
         scope = scope.value
 
-    token.check_scope([f"{config.scope_prefix}{scope}", f"{config.scope_prefix_udts}:{scope}"])
+    return token.check_scope([f"{config.scope_prefix}{scope}", f"{config.scope_prefix_udts}:{scope}"])
 
 
 def has_scope(context: Context, scope: Scopes | str, raise_error: bool = True) -> bool:
