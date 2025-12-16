4b6e2c8be0068e7e8a63fe6564ffd36b53aa2958
Saulius Petraitis <saulius.petraitis@vssa.lt>
2025-12-16 10:07:55 +0200

    fix: Fixed formmating

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx_rtd_theme",
-    "myst_parser", 
-
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
