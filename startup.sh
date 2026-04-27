596fd2069f777ae8215b23aae9ce9bcf7de57178
tutis <tautvydas@cornercasetech.com>
2026-04-27 13:27:19 +0300

    Pypi docker images

--- startup.sh
+++ startup.sh
@@ -2,11 +2,11 @@
 set -x
 
 if [ -z "$(ls -A $HOME/config 2>/dev/null)" ]; then
-    poetry run spinta check
+    spinta check
 fi
 
 if [ "${SPINTA_AUTO_UPGRADE:-false}" = "true" ]; then
-  poetry run spinta upgrade
+  spinta upgrade
 fi
 
-poetry run uvicorn spinta.asgi:app --log-level info --host 0.0.0.0
+uvicorn spinta.asgi:app --log-level info --host 0.0.0.0
